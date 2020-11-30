199083314d11bde96c14e08f58235e067c04e356
olas <olas@cesar.org.br>
2020-11-30 17:25:13 -0300

    small changes in header

--- internal/pemutil/include/pem.h
+++ internal/pemutil/include/pem.h
@@ -7,8 +7,11 @@
 #include <openssl/x509v3.h>
 #include "../../../utils/include/util.h"
 
-//X509 *d2i_X509(X509 **px, const byte **in, long len);
-//PEM_read_bio_X509(BIO *bioptr, NULL, NULL, NULL, NULL);
+// X509 *d2i_X509(X509 **px, const byte **in, long len);
+// PEM_read_bio_X509(BIO *bioptr, NULL, NULL, NULL);
+// PEM_read_bio_PKCS8_PRIV_KEY_INFO(BIO *bioptr, NULL, NULL, NULL);
+// int PEM_read_bio(BIO *bp, char **name, char **header,
+                //   unsigned char **data, long *len);
 
 X509** pemutil_ParseCertificate(const byte *bytes, err_t *err);
 EVP_PKEY* pemutil_ParsePrivateKey(const byte *bytes, err_t *err);
