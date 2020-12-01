8fcec03b3f8ddcbe9aecee545865e05489e000b6
olas <olas@cesar.org.br>
2020-12-01 09:54:29 -0300

    First version of parseBlocks

--- internal/pemutil/src/pem.c
+++ internal/pemutil/src/pem.c
@@ -5,11 +5,28 @@
 
 static const string_t types_str[] = {"CERTIFICATE", "PRIVATE KEY"};
 
-static void* parseBlock(BIO *bio_mem, const string_t type, byte **data, err_t *err)
+static void* parseBlock(BIO *bio_mem, 
+                const string_t type, 
+                err_t *err)
 {
+    /*
+    returns the pointer to parsed PEM as a
+    X509 certificate or PKCS8 private key info.
+    Takes a pointer to BIO, data string for 
+    the type ("CERTIFICATE" or "PRIVATE KEY"), 
+    an out pointer to return the raw data and 
+    an error variable.
+    */
+
+    //generic pointer to be returned
     void *parsed_pem = NULL;
+    //return variable for the pem name
     char *pem_name = NULL;
-    int suc = PEM_read_bio(bio_mem, &pem_name, NULL, data, NULL);
+    //length of the data read from bio_mem
+    long int len_data = 0;
+    //pointer to data
+    byte **data;
+    int suc = PEM_read_bio(bio_mem, &pem_name, NULL, data, &len_data);
     
     *err = NO_ERROR;
     //sucessfully read
@@ -20,15 +37,18 @@ static void* parseBlock(BIO *bio_mem, const string_t type, byte **data, err_t *e
             if(!strcmp(pem_name, types_str[CERT_TYPE]))
             {
                 //read X509 certificate
-                X509 *cert = PEM_read_bio_X509(bio_mem, NULL, NULL, NULL);
+                // X509 *cert = PEM_read_bio_X509(bio_mem, NULL, NULL, NULL);
+                X509 *cert = d2i_X509(NULL, (const byte**) data, len_data);
                 parsed_pem = cert;
                 
             }
             else if(!strcmp(pem_name, types_str[KEY_TYPE]))
             {
                 //read PKCS8 private key
-                PKCS8_PRIV_KEY_INFO *pkey_info =
-                    PEM_read_bio_PKCS8_PRIV_KEY_INFO(bio_mem, NULL, NULL, NULL);
+                // PKCS8_PRIV_KEY_INFO *pkey_info =
+                //     PEM_read_bio_PKCS8_PRIV_KEY_INFO(bio_mem, NULL, NULL, NULL);
+                PKCS8_PRIV_KEY_INFO *pkey_info = 
+                    d2i_PKCS8_PRIV_KEY_INFO(NULL, (const byte**) data, len_data);
                 parsed_pem = pkey_info;
             }
             //PEM type not supported
@@ -47,17 +67,43 @@ static void* parseBlock(BIO *bio_mem, const string_t type, byte **data, err_t *e
     return parsed_pem;
 }
 
-static void** parseBlocks(const byte *pem_byte)
+static void** parseBlocks(const byte *pem_byte, const string_t type, err_t *err)
 {
+    //bio_mem MUST be initialized just one time (??)
+    /** TODO: verify whether BIO keeps track of the
+     * already read data.
+    */
     BIO *bio_mem = BIO_new(BIO_s_mem());
     BIO_puts(bio_mem, pem_byte);
     
     //dynamic array of parsed blocks
     void **parsed_blocks_arr = NULL;
 
-    
-    // dummy
-    return NULL;
+    while(true)
+    {
+        void *block = parseBlock(bio_mem, type, err);
+
+        //type not supported currently
+        if(*err == ERROR1) continue;
+
+        if(block && !err)
+        {
+            arrput(parsed_blocks_arr, block);
+        }
+        else if(!block)
+        {
+            break;
+        }
+        else if(err)
+        {
+            //handle error here
+            arrfree(parsed_blocks_arr);
+            return NULL;
+        }
+    }
+    *err = NO_ERROR;
+
+    return parsed_blocks_arr;
 }
 
 X509** pemutil_ParseCertificate(const byte *bytes, err_t *err)
