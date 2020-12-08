2c753b88c17bd6c508e0734eb793d5e6e537421f
olas <olas@cesar.org.br>
2020-12-08 08:18:29 -0300

    Implementation of x509util_CopyX509Authorities

--- internal/x509util/src/util.c
+++ internal/x509util/src/util.c
@@ -1,8 +1,22 @@
+#include <openssl/x509.h>
 #include "../include/util.h"
 
 X509** x509util_CopyX509Authorities(const X509 **certs)
 {
-    //dummy
+    if(certs)
+    {
+        X509 **new_certs = NULL;
+        for(size_t i = 0, size = arrlenu(certs); i < size; ++i)
+        {
+            X509 *cert = certs[i];
+            //ups the ref count, so it is memory safe
+            //no need to copy the contents, for now
+            X509_up_ref(cert);
+            arrput(new_certs, cert);
+        }
+
+        return new_certs;
+    }
     return NULL;
 }
 
