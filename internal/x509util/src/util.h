637f5731c46a0d7c4f15587c3fbf2c4580ae24b0
olas <olas@cesar.org.br>
2020-12-23 15:10:12 -0300

    Few fixes and function signature changes

--- internal/x509util/src/util.h
+++ internal/x509util/src/util.h
@@ -10,8 +10,8 @@
 func NewCertPool(certs []*x509.Certificate) *x509.CertPool
 */
 
-X509** x509util_CopyX509Authorities(const X509 **certs);
-bool x509util_CertsEqual(const X509 **certs1, const X509 **certs2);
+X509** x509util_CopyX509Authorities(X509 **certs);
+bool x509util_CertsEqual(X509 **certs1, X509 **certs2);
 byte** x509util_RawCertsFromCerts(const X509 **certs);
 byte* x509util_ConcatRawCertsFromCerts(const X509 **certs);
 
