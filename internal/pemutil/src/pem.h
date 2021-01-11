f3d159feb4f8f93a5b970746a4efc4169956d1b5
Thiago Jamir e Silva <tjs@cesar.org.br>
2021-01-11 16:36:27 -0300

    Merge internal and util modules

---
+++ internal/pemutil/src/pem.h
@@ -0,0 +1,38 @@
+#ifndef __INCLUDE_INTERNAL_PEMUTIL_PEM_H__
+#define __INCLUDE_INTERNAL_PEMUTIL_PEM_H__
+
+#include <openssl/pem.h>
+#include <openssl/evp.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include "../../../utils/src/util.h"
+
+typedef struct pemutil_Block
+{
+    string_t type;
+    // map_string_string *headers;
+    byte *bytes;
+} pemutil_Block;
+
+void* parseBlock(BIO *bio_mem, 
+                    const char *type, 
+                    err_t *err);
+
+void** parseBlocks(const byte *pem_byte, 
+                    const char *type, 
+                    err_t *err);
+
+X509** pemutil_ParseCertificates(const byte *bytes, err_t *err);
+EVP_PKEY* pemutil_ParsePrivateKey(const byte *bytes, 
+                                    err_t *err);
+byte* pemutil_EncodePrivateKey(EVP_PKEY *pkey, 
+                                int *bytes_len, 
+                                err_t *err);
+byte** pemutil_EncodeCertificates(X509 **certs);
+
+/*
+func parseBlocks(blocksBytes []byte, expectedType string) ([]interface{}, error)
+func parseBlock(pemBytes []byte, pemType string) (interface{}, []byte, error)
+*/
+
+#endif
---
+++ internal/x509util/src/util.c
@@ -0,0 +1,59 @@
+#include <openssl/x509.h>
+#include "util.h"
+
+X509** x509util_CopyX509Authorities(X509 **certs)
+{
+    if(certs)
+    {
+        X509 **new_certs = NULL;
+        for(size_t i = 0, size = arrlenu(certs); i < size; ++i)
+        {
+            X509 *cert = certs[i];
+            //ups the ref count, so it is memory safe
+            //no need to copy the contents, for now
+            if(cert)
+                X509_up_ref(cert);
+            arrput(new_certs, cert);
+        }
+
+        return new_certs;
+    }
+    return NULL;
+}
+
+bool x509util_CertsEqual(X509 **certs1, X509 **certs2)
+{
+    if(certs1 && certs2)
+    {
+        if(arrlenu(certs1) == arrlenu(certs2))
+        {
+            for(size_t i = 0, size = arrlenu(i); i < size; ++i)
+            {
+                if(X509_cmp(certs1[i], certs2[i]))
+                    return false;
+            }
+
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    return certs1 == certs2;
+}
+
+/**
+ * TODO: how is it different from pemutil_EncodeCertificates?
+ * Answer: it seems it just uses the raw value of the X509 certificate. (??)
+ */
+byte** x509util_RawCertsFromCerts(const X509 **certs)
+{
+    //dummy
+    return NULL;
+}
+
+byte* x509util_ConcatRawCertsFromCerts(const X509 **certs)
+{
+    //dummy
+    return NULL;
+}
