9a08abb388d2cb2638e34ca5192fffdd7031cf63
olas <olas@cesar.org.br>
2021-01-13 14:24:23 -0300

    Certificate Pool

---
+++ internal/x509util/src/certpool.h
@@ -0,0 +1,26 @@
+#ifndef __INCLUDE_INTERNAL_X509UTIL_CERTPOOL_H__
+#define __INCLUDE_INTERNAL_X509UTIL_CERTPOOL_H__
+
+#include <openssl/x509.h>
+#include "../../../utils/src/util.h"
+#include "../../../utils/src/stb_ds.h"
+
+typedef struct map_string_int_arr
+{
+    string_t key;
+    int *value;
+} map_string_int_arr;
+
+typedef struct x509util_CertPool
+{
+    X509 **certs;
+    map_string_int_arr *subj_keyid_idcs;
+    map_string_int_arr *name_idcs;
+} x509util_CertPool;
+
+x509util_CertPool* x509util_CertPool_New(void);
+void x509util_CertPool_AddCert(x509util_CertPool *certpool, X509 *cert);
+bool x509util_CertPool_contains(x509util_CertPool *certpool, const X509 *cert);
+void x509util_CertPoll_Free(x509util_CertPool *certpool);
+
+#endif
