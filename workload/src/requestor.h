7a5043ca8de79440c62d2280c80def3c93d40d51
Rodrigo Lopes <rlc2@cesar.org.br>
2021-01-15 04:51:31 -0300

    added requestor.h from personal branch

--- workload/src/requestor.h
+++ workload/src/requestor.h
@@ -1,15 +1,5 @@
-/*
- * Filename: c-spiffe/requestor/requestor.h
- * Path: c-spiffe/requestor
- * Created Date: Monday, December 21nd 2020, 10:12:28 am
- * Author: Rodrigo Lopes (rlc2@cesar.org.br)
- * 
- * Copyright (c) 2020 CESAR
- */
-
 #ifndef _REQUESTOR_H_
 #define _REQUESTOR_H_
-#include "../../svid/x509svid/src/svid.h"
   #ifdef __cplusplus
 #define EXTERN_C extern "C" {
 #define EXTERN_C_END }
@@ -17,7 +7,42 @@
 #define EXTERN_C
 #define EXTERN_C_END
 #endif
+#include "../../svid/x509svid/src/svid.h"
+#include <pthread.h>
 EXTERN_C
-x509svid_SVID* fetch_SVID();
+typedef struct Requestor {
+    //TODO: keep tabs on threads?
+    //pthread_t** threads;
+
+    const* char address;
+} Requestor;
+
+typedef struct Request{
+  int num_args;
+  void** args;
+} Request;
+
+typedef struct Response{
+  void* response;
+  err_t error;
+} Response;
+typedef struct StreamResponse
+{
+  void* response;
+  pthread_t *thread;
+  err_t error;
+} StreamResponse;
+
+//constructor / destructor for Requestor
+Requestor* RequestorInit(char* address);
+void RequestorFree(Requestor* requestor);
+
+// StreamResponse RequestorRequestStream(Requestor* requestor, Request request, (void*)(*callback)(void*));
+// Response RequestorRequest(Requestor* requestor,Request request);
+
+//wrapper methods from gRPC service definition (see proto/workload.proto)
+x509svid_SVID* FetchDefaultX509SVID(Requestor* requestor); //not in definition
+int FetchAllX509SVID(Requestor* requestor, x509svid_SVID** svids); //not in definition
+// x509svid_SVID_Source* FetchX509SVIDSource(Requestor* requestor);
 EXTERN_C_END
 #endif /* _REQUESTOR_H_ */
