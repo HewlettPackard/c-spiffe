bdae10ac35001665f29b70e0050f2242184b7b2b
olas <olas@cesar.org.br>
2021-01-19 19:02:20 -0300

    Adding new tests

--- utils/test/check_util.c
+++ utils/test/check_util.c
@@ -13,6 +13,9 @@ START_TEST(test_string_new)
 
     ck_assert_str_eq(str2, res_str);
     ck_assert_uint_eq(arrlenu(str2), sizeof res_str);
+
+    arrfree(str1);
+    arrfree(str2);
 }
 END_TEST
 
@@ -28,6 +31,47 @@ START_TEST(test_string_push)
 
     ck_assert_str_eq(str1, res_str);
     ck_assert_uint_eq(arrlenu(str1), sizeof res_str);
+
+    arrfree(str1);
+    arrfree(str2);
+    arrfree(str3);
+}
+END_TEST
+
+START_TEST(test_string_new_range)
+{
+    const char test_str[] = 
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789";
+
+    string_t str1 = string_new_range(test_str, test_str + 10);
+    string_t str2 = string_new_range(test_str + 26, test_str + 36);
+    string_t str3 = string_new_range(test_str + 52, test_str + 62);
+
+    ck_assert_str_eq(str1, "ABCDEFGHIJ");
+    ck_assert_str_eq(str2, "abcdefghij");
+    ck_assert_str_eq(str3, "0123456789");
+
+    arrfree(str1);
+    arrfree(str2);
+    arrfree(str3);
+}
+END_TEST
+
+START_TEST(test_FILE_to_string)
+{
+    FILE *f = fopen("test.txt", "r");
+    string_t buffer = FILE_to_string(f);
+    fclose(f);
+
+    ck_assert_uint_ge(arrlenu(buffer), 63);
+    ck_assert_str_eq(buffer, 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789");
+
+    arrfree(buffer);
 }
 END_TEST
 
@@ -38,6 +82,8 @@ Suite* util_suite(void)
 
     tcase_add_test(tc_core, test_string_new);
     tcase_add_test(tc_core, test_string_push);
+    tcase_add_test(tc_core, test_string_new_range);
+    tcase_add_test(tc_core, test_FILE_to_string);
 
     suite_add_tcase(s, tc_core);
 
