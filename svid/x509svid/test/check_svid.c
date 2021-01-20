c4aa49537f602b33aca2dd80b1d20b380c3a4a6d
olas <olas@cesar.org.br>
2021-01-20 16:07:56 -0300

    Testing x509svid_ParseRaw

--- svid/x509svid/test/check_svid.c
+++ svid/x509svid/test/check_svid.c
@@ -50,6 +50,80 @@ START_TEST(test_x509svid_Parse)
 }
 END_TEST
 
+START_TEST(test_x509svid_ParseRaw)
+{
+    const int ITERS = 4;
+
+    FILE *f = fopen("good-leaf-and-intermediate.pem", "r");
+    string_t buffer = FILE_to_string(f);
+    fclose(f);
+
+    BIO *bio_mem = BIO_new(BIO_s_mem());
+    BIO_puts(bio_mem, buffer);
+    arrfree(buffer);
+
+    unsigned char certs_der_bytes[10000];
+    unsigned char *certs_pout = certs_der_bytes;
+
+    X509 **certs = NULL;
+    for(int i = 0; i < ITERS; ++i)
+    {
+        X509 *cert = PEM_read_bio_X509(bio_mem, NULL, NULL, NULL);
+        if(cert)
+        {
+            i2d_X509(cert, &certs_pout);
+            arrput(certs, cert);
+        }
+    }
+
+    f = fopen("key-pkcs8-ecdsa.pem", "r");
+    buffer = FILE_to_string(f);
+    fclose(f);
+
+    BIO_puts(bio_mem, buffer);
+    arrfree(buffer);
+
+    EVP_PKEY *pkey = PEM_read_bio_PrivateKey(bio_mem, NULL, NULL, NULL);
+
+    unsigned char pkey_der_bytes[10000];
+    unsigned char *pkey_pout = pkey_der_bytes;
+
+    i2d_PrivateKey(pkey, &pkey_pout);
+
+    err_t err;
+    x509svid_SVID *svid = x509svid_ParseRaw(certs_der_bytes, 
+                                            certs_pout - certs_der_bytes,
+                                            pkey_der_bytes,
+                                            pkey_pout - pkey_der_bytes,
+                                            &err);
+
+    ck_assert_uint_eq(err, NO_ERROR);
+    ck_assert_ptr_ne(svid, NULL);
+    ck_assert(x509util_CertsEqual(certs, svid->certs));
+
+    EC_KEY *ec_key1 = EVP_PKEY_get0_EC_KEY(pkey);
+    EC_KEY *ec_key2 = EVP_PKEY_get0_EC_KEY(svid->privateKey);
+
+    const BIGNUM *n1 = EC_KEY_get0_private_key(ec_key1);
+    const EC_GROUP *group1 = EC_KEY_get0_group(ec_key1);
+
+    const BIGNUM *n2 = EC_KEY_get0_private_key(ec_key2);
+    const EC_GROUP *group2 = EC_KEY_get0_group(ec_key2);
+
+    ck_assert_int_eq(BN_cmp(n1, n2), 0);
+    ck_assert_int_eq(EC_GROUP_cmp(group1, group2, NULL), 0);
+
+    for(size_t i = 0, size = arrlenu(certs); i < size; ++i)
+    {
+        X509_free(certs[i]);
+    }
+    arrfree(certs);
+    EVP_PKEY_free(pkey);
+    BIO_free(bio_mem);
+    x509svid_SVID_Free(svid, true);
+}
+END_TEST
+
 START_TEST(test_x509svid_newSVID)
 {
     FILE *f = fopen("good-leaf-and-intermediate.pem", "r");
@@ -308,6 +382,7 @@ Suite* svid_suite(void)
 
     tcase_add_test(tc_core, test_x509svid_Load);
     tcase_add_test(tc_core, test_x509svid_Parse);
+    tcase_add_test(tc_core, test_x509svid_ParseRaw);
     tcase_add_test(tc_core, test_x509svid_newSVID);
     tcase_add_test(tc_core, test_x509svid_validateCertificates);
     tcase_add_test(tc_core, test_x509svid_validateLeafCertificate);
