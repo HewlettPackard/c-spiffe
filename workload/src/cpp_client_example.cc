f5711cb3b1a8544ba8ef3837973192c7163e1053
Rodrigo Lopes <rlc2@cesar.org.br>
2021-01-21 11:33:31 -0300

    renamed examples and added build targets using requestor as a library

--- workload/src/cpp_client_example.cc
+++ workload/src/cpp_client_example.cc
@@ -19,8 +19,9 @@ int main(int argc, char const *argv[])
     Requestor* requestor = RequestorInit("unix:///tmp/agent.sock");
     x509svid_SVID* svid = FetchDefaultX509SVID(requestor);
     if(svid){
-        std::cout << "id:" << svid << std::endl << svid->id.path << std::endl << svid->id.td.name << std::endl << svid->certs << std::endl << svid->privateKey;
+        std::cout << "address:" << svid << std::endl <<"SVID Path: " << svid->id.path << std::endl << "Trust Domain: " << svid->id.td.name << std::endl << "Cert(s) Address: " << svid->certs << std::endl << "Key Address: " << svid->privateKey << std::endl;
     }
     RequestorFree(requestor);
+    x509svid_SVID_Free(svid,true);
     return 0;
 }
