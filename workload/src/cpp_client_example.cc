6c2b53f0795ed31f00a3deb379847b826c69bfab
Rodrigo Lopes <rlc2@cesar.org.br>
2021-01-21 11:47:27 -0300

    removed address printing from if code block

--- workload/src/cpp_client_example.cc
+++ workload/src/cpp_client_example.cc
@@ -7,8 +7,9 @@ int main(int argc, char const *argv[])
 {
     Requestor* requestor = RequestorInit("unix:///tmp/agent.sock");
     x509svid_SVID* svid = FetchDefaultX509SVID(requestor);
+    std::cout << "Address:" << svid << std::endl;
     if(svid){
-        std::cout << "Address:" << svid << std::endl <<"SVID Path: " << svid->id.path << std::endl << "Trust Domain: " << svid->id.td.name << std::endl << "Cert(s) Address: " << svid->certs << std::endl << "Key Address: " << svid->privateKey << std::endl;
+        std::cout <<"SVID Path: " << svid->id.path << std::endl << "Trust Domain: " << svid->id.td.name << std::endl << "Cert(s) Address: " << svid->certs << std::endl << "Key Address: " << svid->privateKey << std::endl;
     }
     RequestorFree(requestor);
     x509svid_SVID_Free(svid,true);
