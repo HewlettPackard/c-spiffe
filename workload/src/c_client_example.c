de9606209f5bb8ad776764954c0ecde788a09876
Rodrigo <rlc2@cesar.org.br>
2021-02-11 13:23:21 -0300

    fixed client usage and added error output

--- workload/src/c_client_example.c
+++ workload/src/c_client_example.c
@@ -5,7 +5,18 @@
 int main(void) {
   err_t error = NO_ERROR;
   workloadapi_Client *client = workloadapi_NewClient(&error);
+  if(error != NO_ERROR) {
+    printf("client error! %d\n",(int)error);
+  }
+  workloadapi_defaultClientOptions(client,NULL);
+  error = workloadapi_ConnectClient(client);
+  if(error != NO_ERROR) {
+    printf("conn error! %d\n",(int)error);
+  }
   x509svid_SVID *svid = workloadapi_FetchX509SVID(client,&error);
+  if(error != NO_ERROR) {
+    printf("fetch error! %d\n",(int)error);
+  }
   printf("Address : %p\n", svid);
 
   if (svid) {
--- workload/src/cpp_client_example.cc
+++ workload/src/cpp_client_example.cc
@@ -3,12 +3,28 @@
 #include <cstdlib>
 #include <iostream>
 
-int main(void) {
+int main(void)
+{
   err_t error = NO_ERROR;
   workloadapi_Client *client = workloadapi_NewClient(&error);
-  x509svid_SVID *svid = workloadapi_FetchX509SVID(client,&error);
- std::cout << "Address:" << svid << std::endl;
-  if (svid) {
+  if (error != NO_ERROR)
+  {
+    std::cout << "client error! " << (int)error <<std::endl;
+  }
+  workloadapi_defaultClientOptions(client, NULL);
+  error = workloadapi_ConnectClient(client);
+  if (error != NO_ERROR)
+  {
+    std::cout << "conn error! " << (int)error <<std::endl;
+  }
+  x509svid_SVID *svid = workloadapi_FetchX509SVID(client, &error);
+  if (error != NO_ERROR)
+  {
+    std::cout << "fetch error! " << (int)error <<std::endl;
+  }
+  std::cout << "Address:" << svid << std::endl;
+  if (svid)
+  {
     std::cout << "SVID Path: " << svid->id.path << std::endl
               << "Trust Domain: " << svid->id.td.name << std::endl
               << "Cert(s) Address: " << svid->certs << std::endl
