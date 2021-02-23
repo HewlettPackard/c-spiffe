dcf8a6bd8a56fcf5bc02b35cd9965ae6f2ed3f4b
otaviolcs3 <olas@cin.ufpe.br>
2021-02-23 11:41:34 -0300

    Fixing identation

--- internal/jwtutil/src/util.h
+++ internal/jwtutil/src/util.h
@@ -9,25 +9,25 @@ extern "C"
 {
 #endif
 
-    /**
-     * Copy a stb string hash map of jwt authorities.
-     *
-     * \param hash [in] stb string hash map of public keys.
-     * \returns A copy of hash. Must be freed iterating over hash using
-     * EVP_PKEY_free and then shfree on the map.
-     */
-    map_string_EVP_PKEY *jwtutil_CopyJWTAuthorities(map_string_EVP_PKEY *hash);
+/**
+ * Copy a stb string hash map of jwt authorities.
+ *
+ * \param hash [in] stb string hash map of public keys.
+ * \returns A copy of hash. Must be freed iterating over hash using
+ * EVP_PKEY_free and then shfree on the map.
+ */
+map_string_EVP_PKEY *jwtutil_CopyJWTAuthorities(map_string_EVP_PKEY *hash);
 
-    /**
-     * Compares two stb string hash maps of jwt authorities.
-     *
-     * \param hash1 [in] First stb string hash map of public keys.
-     * \param hash2 [in] Second stb string hash map of public keys.
-     * \returns <tt>true</tt> if the maps have the same values for the same
-     * keys, <tt>false</tt> otherwise.
-     */
-    bool jwtutil_JWTAuthoritiesEqual(map_string_EVP_PKEY *hash1,
-                                     map_string_EVP_PKEY *hash2);
+/**
+ * Compares two stb string hash maps of jwt authorities.
+ *
+ * \param hash1 [in] First stb string hash map of public keys.
+ * \param hash2 [in] Second stb string hash map of public keys.
+ * \returns <tt>true</tt> if the maps have the same values for the same
+ * keys, <tt>false</tt> otherwise.
+ */
+bool jwtutil_JWTAuthoritiesEqual(map_string_EVP_PKEY *hash1,
+                                map_string_EVP_PKEY *hash2);
 
 #ifdef __cplusplus
 }
