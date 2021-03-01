06f2a2ac301ab1e80d066e8b098615a522175617
Rodrigo <rlc2@cesar.org.br>
2021-03-01 15:20:22 -0300

    added jwt callback struct

---
+++ workload/src/JWTcallback.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDE_WORKLOAD_JWTCALLBACK_H
+#define INCLUDE_WORKLOAD_JWTCALLBACK_H
+
+#include "../../bundle/jwtbundle/src/set.h"
+#include "../../svid/jwtsvid/src/svid.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/** type for callback function. will be set by JWTSource. */
+typedef void(workloadapi_jwtBundleSetFunc_t)(jwtbundle_Set*);
+
+typedef struct {
+    void *args;
+    workloadapi_jwtBundleSetFunc_t func;
+} workloadapi_JWTCallback;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // INCLUDE_WORKLOAD_JWTCALLBACK_H
