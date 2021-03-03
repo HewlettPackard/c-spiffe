3f2e73af7c990c5fc0d7d8e1172483c48398d7be
Ariana <alg@cesar.org.br>
2021-03-03 10:27:33 -0300

    #236 Review fetch x509 Bundle and SVID Missing Automated Test cases (#22)
    * remove unused import
    
    * move utils.py, remove deprecated methods, create a method to update 'server.conf' file and update requirements.txt
    
    * create and implement first scenarios for Watch X509 Bundle
    
    * refactor to correct typo and title inconsistency with the implemented step
    
    * refactor watch_x509_bundle tests due to refactor in previous commit
    
    * new tests for watch x509 bundle and refactor in the old tests
    
    * auxiliary scripts
    
    * update .gitignore
    
    * update .gitignore
    
    * add new scenarios for Fetch X509
    
    * WIP refactoring how server and agent are turned on/off
    
    * refactor tests setup
    
    * finish missing scenarios and adjust review suggested
    
    * restore watch background
    
    * claning the code

--- integration_test/features/environment.py
+++ integration_test/features/environment.py
@@ -1,10 +1,25 @@
 import os
-import sys
-import json
-import argparse
-
-from urllib.parse import unquote
+import time
 
 
 def before_all(context):
-    context.spiffe_id        = context.config.userdata['spiffe_id']
+    context.spiffe_id = context.config.userdata['spiffe_id']
+    context.server_conf = context.config.userdata['server_conf']
+    
+    os.system("./grpc_start_server.sh")
+    time.sleep(5)
+    os.system("./grpc_create_entries.sh")
+    time.sleep(1)
+    os.system("./grpc_generate_token.sh")
+    time.sleep(5)
+    os.system("./grpc_connect_agent.sh")
+    time.sleep(5)
+
+
+def after_all(context):
+    os.system("pkill spire-agent")
+    time.sleep(5)
+    os.system("pkill spire-server")
+    time.sleep(5)
+    os.system("./clean.sh")
+    time.sleep(1)
