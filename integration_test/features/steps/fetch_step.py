7dcc77d5f750c706611fb20449ab79080af2eae7
Ariana <alg@cesar.org.br>
2021-03-05 17:17:52 -0300

    refactor fetch step to be generic with the profiles

--- integration_test/features/steps/fetch_step.py
+++ integration_test/features/steps/fetch_step.py
@@ -1,28 +1,37 @@
 import os
 import sys
-import json
-import base64
-import subprocess
 import time
+
+from hamcrest import assert_that, is_, is_not
+
 
 PARENT_PATH = os.path.abspath("..")
 if PARENT_PATH not in sys.path:
     sys.path.insert(0, PARENT_PATH)
 
-from hamcrest import assert_that, is_, is_not
 
-
-@when('I fetch SVID')
-def step_impl(context):
-    c_client_bin = os.popen("../build/workload/c_client")
-    result = c_client_bin.read()
-    result = result.splitlines()[0]
-    context.svid = result.replace("Address : ", "")
+@when('I fetch "{profile}" SVID')
+def step_impl(context, profile):
+    c_client_bin = os.popen("../build/workload/c_client svid_type=%s" % profile.lower())
+    context.result = c_client_bin.read()
 
 
 @then('I check that the SVID is returned correctly')
 def step_impl(context):
+    assert_that(context.result.find("error"), is_(-1), "There was an error")
+    assert_that(context.result.find("Address : "), is_not(-1), "There is no Address")
+    result = context.result.splitlines()
+    context.svid = result[0].split(" ")[-1]
     assert_that(context.svid, is_not("(nil)"))
+
+
+@then('I check that the SVID is not returned')
+def step_impl(context):
+    assert_that(context.result.find("error"), is_not(-1), "There was no error")
+    assert_that(context.result.find("Address : "), is_not(-1), "There is no Address")
+    result = context.result.splitlines()
+    context.svid = result[1].split(" ")[-1]
+    assert_that(context.svid, is_("(nil)"))
 
 
 @when('I fetch Bundle')
@@ -31,7 +40,7 @@ def step_impl(context):
     result = c_client_bin.read()
     result = result.splitlines()[0]
     context.bundle = result.replace("Address : ", "")
-    
+
 
 @then('I check that the Bundle is returned correctly')
 def step_impl(context):
@@ -50,11 +59,6 @@ def step_impl(context):
     time.sleep(5)
     os.system("./grpc_connect_agent.sh")
     time.sleep(5)
-
-
-@then('I check that the SVID is not returned')
-def step_impl(context):
-    assert_that(context.svid, is_("(nil)"))
 
 
 @then('I check that the Bundle is not returned')
