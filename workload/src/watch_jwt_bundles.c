cc6a8f9b5dd40a5b9e3a458dd3bda323c8583db9
rodrigolc <rodrigolpscarvalho@gmail.com>
2021-03-11 10:24:20 -0300

    fixed counter types and fixed formatting

--- workload/src/watch_jwt_bundles.c
+++ workload/src/watch_jwt_bundles.c
@@ -1,30 +1,31 @@
 #include "../../workload/src/jwtwatcher.h"
 
-
-
-void print_function(jwtbundle_Set* set, void* not_used){
+void print_function(jwtbundle_Set *set, void *not_used)
+{
     BIO *out;
     out = BIO_new_fd(fileno(stdout), BIO_NOCLOSE);
 
-    for (int i = 0, size = jwtbundle_Set_Len(set); i < size; i++){
-        printf("TD name: %s\n",set->bundles[i].value->td.name);
-        
-        for(int j = 0, size2 = hmlen(set->bundles[i].value->auths); j < size2; j++){
-            printf(" kID: %s\n",set->bundles[i].value->auths[j].key);
-            EVP_PKEY_print_params(out,set->bundles[i].value->auths[j].value,2,NULL);
-            EVP_PKEY_print_public(out,set->bundles[i].value->auths[j].value,2,NULL);
+    for(uint32_t i = 0, size = jwtbundle_Set_Len(set); i < size; ++i) {
+        printf("TD name: %s\n", set->bundles[i].value->td.name);
+
+        for(size_t j = 0, size2 = hmlenu(set->bundles[i].value->auths);
+            j < size2; ++j) {
+            printf(" kID: %s\n", set->bundles[i].value->auths[j].key);
+            EVP_PKEY_print_params(out, set->bundles[i].value->auths[j].value,
+                                  2, NULL);
+            EVP_PKEY_print_public(out, set->bundles[i].value->auths[j].value,
+                                  2, NULL);
         }
     }
     BIO_free(out);
 }
-
 
 int main(int argc, char const *argv[])
 {
     workloadapi_JWTWatcherConfig config;
     config.client = NULL;
     config.client_options = NULL;
-    arrpush(config.client_options,workloadapi_Client_defaultOptions);
+    arrpush(config.client_options, workloadapi_Client_defaultOptions);
 
     workloadapi_JWTCallback cb;
     cb.args = NULL;
@@ -32,31 +33,32 @@ int main(int argc, char const *argv[])
 
     err_t error = NO_ERROR;
 
-    workloadapi_JWTWatcher* watcher = workloadapi_newJWTWatcher(config,cb,&error);
+    workloadapi_JWTWatcher *watcher
+        = workloadapi_newJWTWatcher(config, cb, &error);
 
-    if(error){
-        printf("error %d on newJWTWatcher()\n",error);
+    if(error) {
+        printf("error %d on newJWTWatcher()\n", error);
     }
     printf("press Enter to stop.\n");
     error = workloadapi_JWTWatcher_Start(watcher);
 
-    if(error){
-        printf("error %d on JWTWatcher_Start()\n",error);
+    if(error) {
+        printf("error %d on JWTWatcher_Start()\n", error);
     }
     char ch;
-    scanf("%c",&ch);
-    
+    scanf("%c", &ch);
+
     printf("Stopping.\n");
-    
+
     error = workloadapi_JWTWatcher_Close(watcher);
-    if(error){
-        printf("error %d on JWTWatcher_Close()\n",error);
+    if(error) {
+        printf("error %d on JWTWatcher_Close()\n", error);
     }
     error = workloadapi_JWTWatcher_Free(watcher);
-    if(error){
-        printf("error %d on JWTWatcher_Free()\n",error);
+    if(error) {
+        printf("error %d on JWTWatcher_Free()\n", error);
     }
-    
+
     arrfree(config.client_options);
 
     return 0;
