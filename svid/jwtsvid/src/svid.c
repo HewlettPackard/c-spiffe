bacdf9115b0e287c80d6c921bfe768d79de80f68
Willian Alves <wra@cesar.org.br>
2021-03-19 10:40:33 -0400

    adjust jwtsvid module

--- svid/jwtsvid/src/svid.c
+++ svid/jwtsvid/src/svid.c
@@ -1,5 +1,5 @@
-#include "svid.h"
-#include "../../../bundle/jwtbundle/src/source.h"
+#include "svid/jwtsvid/src/svid.h"
+#include "bundle/jwtbundle/src/source.h"
 #include <cjose/cjose.h>
 
 // one minute leeway
