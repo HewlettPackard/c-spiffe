da0091c32bda73b25344d8dbeb8e0209995146c0
Willian Alves <wra@cesar.org.br>
2021-03-22 11:40:29 -0400

    Adjust in infra

--- infra/spire-server/create-entries.sh
+++ infra/spire-server/create-entries.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
-./bin/spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/workload -selector unix:uid:1001 -ttl 3600
-./bin/spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:uid:1002 -ttl 3600
+spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/workload -selector unix:uid:1001 -ttl 3600
+spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:uid:1002 -ttl 3600
 
