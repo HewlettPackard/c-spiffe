e5ba6a70fd29a446d541e1e7b4647a4099003ca8
Willian Alves <wra@cesar.org.br>
2021-03-23 11:41:40 -0400

    Adjusted creating entries and Makefile, added user on dockerfile tests

--- infra/spire-server/create-entries.sh
+++ infra/spire-server/create-entries.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -x
-spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/workload -selector unix:uid:1001 -ttl 3600
-spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:uid:1002 -ttl 3600
-
+spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:uid:1002
+spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:user:root
