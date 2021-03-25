d5c08ae4b7ccafb9e12198c27b982759e40ea64e
Willian Alves <wra@cesar.org.br>
2021-03-25 18:09:58 -0400

    Added integrations tests on infrastruture

--- infra/spire-server/create-entries.sh
+++ infra/spire-server/create-entries.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -x
+spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkload -selector unix:user:root
 spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:uid:1002
 spire-server entry create -parentID spiffe://example.org/host -spiffeID spiffe://example.org/tests -selector unix:user:root
