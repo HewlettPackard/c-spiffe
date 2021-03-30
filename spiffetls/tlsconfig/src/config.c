91819579da4daed01f0a8aa0baa08a246547f593
otaviolcs3 <olas@cin.ufpe.br>
2021-03-30 15:53:13 -0300

    Dev

--- spiffetls/tlsconfig/src/config.c
+++ spiffetls/tlsconfig/src/config.c
@@ -1,6 +1,8 @@
 #include "config.h"
+#include "authorizer.h"
+#include "svid/x509svid/src/verify.h"
 
-tlsconfig_Trace *__trace;
+// tlsconfig_Trace *__trace;
 
 void tlsconfig_Option_apply(tlsconfig_Option *op, tlsconfig_options *options)
 {
@@ -29,18 +31,134 @@ tlsconfig_options *tlsconfig_newOptions(tlsconfig_Option **opts)
     return out;
 }
 
-static void setTrace(tlsconfig_options *opts) { opts->trace = __trace; }
+// static void setTrace(tlsconfig_options *opts) { opts->trace = __trace; }
 
-tlsconfig_Option *tlsconfig_WithTrace(const tlsconfig_Trace *trace)
-{
-    __trace = trace;
-    return tlsconfig_OptionFromFunc(setTrace);
-}
+// tlsconfig_Option *tlsconfig_WithTrace(tlsconfig_Trace *trace)
+// {
+//     __trace = trace;
+//     return tlsconfig_OptionFromFunc(setTrace);
+// }
 
 // func TLSClientConfig(bundle x509bundle.Source, authorizer Authorizer, opts
 // ...Option) *tls.Config {
-SSL *tlsconfig_TLSClientConfig(x509bundle_Source *bundle,
-                               tlsconfig_Authorizer *authorizer,
-                               tlsconfig_Option **opts)
+SSL_CTX *tlsconfig_TLSClientConfig(x509bundle_Source *bundle,
+                                   tlsconfig_Authorizer *authorizer,
+                                   tlsconfig_Option **opts)
 {
+    // dummy
+    return NULL;
+}
+
+struct hookTLSClientConfig_st {
+    x509bundle_Source *bundle;
+    tlsconfig_Authorizer *authorizer;
+    tlsconfig_Option **opts;
+};
+
+/// TODO: change function name
+static int hookTLSClientConfig_cb(X509_STORE_CTX *store_ctx, void *arg)
+{
+    struct hookTLSClientConfig_st *config = arg;
+    spiffeid_ID id;
+    bool suc = x509svid_Verify_cb(store_ctx, config->bundle, &id);
+
+    if(suc) {
+        return tlsconfig_ApplyAuthorizer(config->authorizer, id,
+                                         /*unused arg*/ NULL)
+                       == MATCH_OK
+                   ? 1
+                   : 0;
+    } else {
+        return 0;
+    }
+}
+
+void tlsconfig_HookTLSClientConfig(SSL_CTX *ctx, x509bundle_Source *bundle,
+                                   tlsconfig_Authorizer *authorizer,
+                                   tlsconfig_Option **opts)
+{
+    tlsconfig_resetAuthFields(ctx);
+    /// WARNING: temporary solution
+
+    struct hookTLSClientConfig_st *safe_arg = malloc(sizeof *safe_arg);
+    safe_arg->bundle = bundle;
+    safe_arg->authorizer = authorizer;
+    safe_arg->opts = opts;
+
+    SSL_CTX_set_cert_verify_callback(ctx, hookTLSClientConfig_cb, safe_arg);
+}
+
+struct hookMTLSClientConfig_st {
+    x509svid_Source *svid;
+    x509bundle_Source *bundle;
+    tlsconfig_Authorizer *authorizer;
+    tlsconfig_Option **opts;
+};
+
+/// TODO: change function name
+static int hookMTLSClientConfig_cb(SSL *ssl, X509 **cert, EVP_PKEY **pkey)
+{
+    /// TODO: change to a global variable
+    struct hookMTLSClientConfig_st *config = NULL;
+
+    err_t err;
+    x509svid_SVID *svid = x509svid_Source_GetX509SVID(config->svid, &err);
+
+    if(!err && svid) {
+        /// TODO: check if it is needed to up the reference
+        *cert = svid->certs[0];
+        /// TODO: check if it is needed to up the reference
+        *pkey = svid->private_key;
+
+        return 1;
+    }
+
+    return 0;
+}
+
+void tlsconfig_HookMTLSClientConfig(SSL_CTX *ctx, x509svid_Source *svid,
+                                    x509bundle_Source *bundle,
+                                    tlsconfig_Authorizer *authorizer,
+                                    tlsconfig_Option **opts)
+{
+    tlsconfig_resetAuthFields(ctx);
+    /// WARNING: temporary solution
+
+    struct hookMTLSClientConfig_st *safe_arg0 = malloc(sizeof *safe_arg0);
+    safe_arg0->svid = svid;
+    safe_arg0->bundle = bundle;
+    safe_arg0->authorizer = authorizer;
+    safe_arg0->opts = opts;
+    SSL_CTX_set_client_cert_cb(ctx, hookMTLSClientConfig_cb);
+
+    struct hookTLSClientConfig_st *safe_arg1 = malloc(sizeof *safe_arg1);
+    safe_arg1->bundle = bundle;
+    safe_arg1->authorizer = authorizer;
+    safe_arg1->opts = opts;
+    SSL_CTX_set_cert_verify_callback(ctx, hookTLSClientConfig_cb, safe_arg1);
+}
+
+void tlsconfig_HookMTLSWebClientConfig(SSL_CTX *ctx, x509svid_Source *svid,
+                                       x509util_CertPool *roots,
+                                       tlsconfig_Option **opts)
+{}
+
+void tlsconfig_resetAuthFields(SSL_CTX *ctx)
+{
+    // config.Certificates = nil - ok
+    // config.ClientAuth = tls.NoClientCert
+    // config.GetCertificate = nil - ok
+    // config.GetClientCertificate = nil - ok
+    // config.InsecureSkipVerify = false - ok
+    // config.NameToCertificate = nil //nolint:staticcheck // setting to nil is
+    // OK config.RootCAs = nil
+
+    // clear certificates chain
+    SSL_CTX_clear_chain_certs(ctx);
+    // no callback function set
+    SSL_CTX_set_client_hello_cb(ctx, NULL, NULL);
+    // no callback function set
+    SSL_CTX_set_client_cert_cb(ctx, NULL);
+    // set default verification
+    SSL_CTX_set_cert_verify_callback(ctx, NULL, NULL);
 }
--- svid/x509svid/src/source.c
+++ svid/x509svid/src/source.c
@@ -1,13 +1,37 @@
 #include "source.h"
 
+x509svid_SVID *x509svid_Source_GetX509SVID(x509svid_Source *source, err_t *err)
+{
+    x509svid_SVID *svid = NULL;
+
+    if(source) {
+        if(source->type == X509SVID_SVID) {
+            svid = source->source.svid;
+        } else if(source->type == WORKLOADAPI_X509SOURCE_SVID) {
+            /// TODO: fix circular dependency
+            // svid = workloadapi_X509Source_GetX509SVID(source->source.source,
+            //                                           err);
+        } else {
+            // unknown type
+            *err = ERROR2;
+        }
+    } else {
+        // source is NULL
+        *err = ERROR1;
+    }
+
+    return svid;
+}
+
 void x509svid_Source_Free(x509svid_Source *source)
 {
     if(source) {
         if(source->type == X509SVID_SVID) {
             x509svid_SVID_Free(source->source.svid);
         } else if(source->type == WORKLOADAPI_X509SOURCE_SVID) {
+            /// TODO: fix circular dependency
             // workloadapi_X509Source_Free(source->source.source);
-        }     
+        }
 
         free(source);
     }
