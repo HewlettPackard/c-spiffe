9b230d8e116e6a9801177032eb1cccf1ed84463a
Ariana <alg@cesar.org.br>
2021-04-13 11:07:18 -0300

    remove unused verification and fix path

--- integration_test/features/environment.py
+++ integration_test/features/environment.py
@@ -5,21 +5,19 @@ import sys
 from pathlib2 import Path
 
 
-PARENT_PATH = os.path.abspath("..")
-if PARENT_PATH not in sys.path:
-    sys.path.insert(0, PARENT_PATH)
+PARENT_PATH = os.path.abspath("..") + "integration_test/helpers/"
 
 
 def before_all(context):
     context.spiffe_id = context.config.userdata['spiffe_id']
     context.server_conf = context.config.userdata['server_conf']
     
-    os.system(PARENT_PATH + "/helpers/bash-spire-scripts/grpc_connect_agent.sh")
+    os.system(PARENT_PATH + "bash-spire-scripts/grpc_connect_agent.sh")
     time.sleep(5)
 
 
 def after_all(context):
     os.system("pkill spire-agent")
     time.sleep(5)
-    os.system(PARENT_PATH + "/helpers/bash-general-scripts/clean.sh")
+    os.system(PARENT_PATH + "bash-general-scripts/clean.sh")
     time.sleep(1)
