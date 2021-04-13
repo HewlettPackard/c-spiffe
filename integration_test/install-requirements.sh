4887247d52341ae159f27d5422f4d17840829ea1
Ariana <alg@cesar.org.br>
2021-04-13 10:34:13 -0300

    changing bash scripts organization

--- integration_test/install-requirements.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update
-yes Y | apt-get install python3-pip
-python3 -m pip install -r requirements.txt
-chmod 777 /mnt/integration_test/grpc_conn_test_agent.sh  /mnt/integration_test/grpc_conn_test_entries.sh  /mnt/integration_test/grpc_conn_test_server.sh
