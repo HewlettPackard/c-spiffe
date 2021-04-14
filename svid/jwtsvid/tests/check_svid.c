8e339a2199af9962ebd75bbbe4ae2253bc46f85b
Willian Alves <wra@cesar.org.br>
2021-04-14 17:29:29 -0400

    Added svid_Marshal test

--- svid/jwtsvid/tests/check_svid.c
+++ svid/jwtsvid/tests/check_svid.c
@@ -189,6 +189,50 @@ START_TEST(test_jwtsvid_EC)
 }
 END_TEST
 
+START_TEST(test_jwtsvid_Marshal)
+    spiffeid_TrustDomain td = { "example.com" };
+    jwtbundle_Bundle *bundle = jwtbundle_New(td);
+
+    FILE *f = fopen("./resources/ec-secp256k1-priv-key.pem", "r");
+    ck_assert_ptr_ne(f, NULL);
+    EVP_PKEY *pkey = PEM_read_PrivateKey(f, NULL, NULL, NULL);
+    fclose(f);
+
+    err_t err = jwtbundle_Bundle_AddJWTAuthority(
+        bundle, "ff3c5c96-392e-46ef-a839-6ff16027af78", pkey);
+
+    ck_assert_uint_eq(err, NO_ERROR);
+
+    char token[]
+        = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZmM2M1Yzk2LTM5MmUtNDZlZi1hODM5LTZmZjE2MDI3YWY3OCJ9.eyJzdWIiOiJzcGlmZmU6Ly9leGFtcGxlLmNvbS93b3JrbG9hZDEiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTYyMDAwMDAwMH0.WA7x2GiNZvh5BoLkvS7BBGIHz6ULTCsX7DBJo8kDoPla4wbo4G2157WWCZLx6zPE8Qpvvb11kMk0Ivk_G0gMeA";
+    jwtbundle_Source *source = jwtbundle_SourceFromBundle(bundle);
+    jwtsvid_SVID *svid = jwtsvid_ParseAndValidate(token, source, NULL, &err);
+
+    ck_assert_uint_eq(err, NO_ERROR);
+    ck_assert_ptr_eq(svid->audience, NULL);
+    ck_assert_ptr_ne(svid->claims, NULL);
+    ck_assert_uint_eq(shlenu(svid->claims), 4);
+    ck_assert_int_ge(shgeti(svid->claims, "sub"), 0);
+    ck_assert_int_ge(shgeti(svid->claims, "name"), 0);
+    ck_assert_int_ge(shgeti(svid->claims, "iat"), 0);
+    ck_assert_int_ge(shgeti(svid->claims, "exp"), 0);
+    ck_assert_int_eq(svid->expiry, 1620000000);
+    ck_assert_ptr_ne(svid->id.path, NULL);
+    ck_assert_str_eq(svid->id.path, "/workload1");
+    ck_assert_ptr_ne(svid->id.td.name, NULL);
+    ck_assert_str_eq(svid->id.td.name, "example.com");
+    ck_assert_ptr_ne(svid->token, NULL);
+    ck_assert_str_eq(svid->token, token);
+
+    const char *marshal = jwtsvid_SVID_Marshal(svid);
+
+    ck_assert_ptr_ne(marshal, NULL);
+
+    jwtbundle_Source_Free(source);
+    EVP_PKEY_free(pkey);
+    jwtsvid_SVID_Free(svid);
+END_TEST
+
 Suite *svid_suite(void)
 {
     Suite *s = suite_create("svid");
@@ -198,6 +242,7 @@ Suite *svid_suite(void)
     tcase_add_test(tc_core, test_jwtsvid_ParseInsecure);
     tcase_add_test(tc_core, test_jwtsvid_ParseAndValidate);
     tcase_add_test(tc_core, test_jwtsvid_EC);
+    tcase_add_test(tc_core, test_jwtsvid_Marshal);
 
     suite_add_tcase(s, tc_core);
 
