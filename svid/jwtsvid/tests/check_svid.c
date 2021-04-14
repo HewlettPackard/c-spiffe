38213f3342405a856b5da956728a5a26213cca3c
Willian Alves <wra@cesar.org.br>
2021-04-14 18:49:45 -0400

    Added test for error5

--- svid/jwtsvid/tests/check_svid.c
+++ svid/jwtsvid/tests/check_svid.c
@@ -163,7 +163,11 @@ START_TEST(test_jwtsvid_EC)
     ck_assert_uint_eq(err, NO_ERROR);
 
     char token[]
-        = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZmM2M1Yzk2LTM5MmUtNDZlZi1hODM5LTZmZjE2MDI3YWY3OCJ9.eyJzdWIiOiJzcGlmZmU6Ly9leGFtcGxlLmNvbS93b3JrbG9hZDEiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTYyMDAwMDAwMH0.WA7x2GiNZvh5BoLkvS7BBGIHz6ULTCsX7DBJo8kDoPla4wbo4G2157WWCZLx6zPE8Qpvvb11kMk0Ivk_G0gMeA";
+        = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZmM2M1Yzk2LTM5MmUtNDZlZ"
+          "i1hODM5LTZmZjE2MDI3YWY3OCJ9."
+          "eyJzdWIiOiJzcGlmZmU6Ly9leGFtcGxlLmNvbS93b3JrbG9hZDEiLCJuYW1lIjoiSm9obi"
+          "BEb2UiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTYyMDAwMDAwMH0.WA7x2GiNZvh5BoLk"
+          "vS7BBGIHz6ULTCsX7DBJo8kDoPla4wbo4G2157WWCZLx6zPE8Qpvvb11kMk0Ivk_G0gMeA";
     jwtbundle_Source *source = jwtbundle_SourceFromBundle(bundle);
     jwtsvid_SVID *svid = jwtsvid_ParseAndValidate(token, source, NULL, &err);
 
@@ -238,6 +242,38 @@ START_TEST(test_jwtsvid_Marshal)
 }
 END_TEST
 
+START_TEST(test_jwtsvid_error5)
+{
+    spiffeid_TrustDomain td = { "example.com" };
+    jwtbundle_Bundle *bundle = jwtbundle_New(td);
+
+    FILE *f = fopen("./resources/ec-secp256k1-priv-key.pem", "r");
+    ck_assert_ptr_ne(f, NULL);
+    EVP_PKEY *pkey = PEM_read_PrivateKey(f, NULL, NULL, NULL);
+    fclose(f);
+
+    err_t err = jwtbundle_Bundle_AddJWTAuthority(
+        bundle, "ff3c5c96-392e-46ef-a839-6ff16027af78", pkey);
+
+    ck_assert_uint_eq(err, NO_ERROR);
+
+    char token[]
+        = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZmM2M1Yzk2LTM5MmUtNDZlZ"
+          "i1hODM5LTZmZjE2MDI3YWY3OCJ9."
+          "eyJzdWIiOiJzcGlmZmU6Ly9leGFtcGxlLmNvbS93b3JrbG9hZDEiLCJuYW1lIjoiSm9obi"
+          "BEb2UiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTYyMDAwMDAwMH0.WA7x2GiNZvh5BoLk"
+          "vS7BBGIHz6ULTCsX7DBJo8kDoPla4wbo4G2157WWCZLx6zPE8Qpvvb11kMk0Ivk";
+    jwtbundle_Source *source = jwtbundle_SourceFromBundle(bundle);
+    jwtsvid_SVID *svid = jwtsvid_ParseAndValidate(token, source, NULL, &err);
+
+    ck_assert_uint_eq(err, ERROR5);
+
+    jwtbundle_Source_Free(source);
+    EVP_PKEY_free(pkey);
+    jwtsvid_SVID_Free(svid);
+}
+END_TEST
+
 Suite *svid_suite(void)
 {
     Suite *s = suite_create("svid");
@@ -248,6 +284,7 @@ Suite *svid_suite(void)
     tcase_add_test(tc_core, test_jwtsvid_ParseAndValidate);
     tcase_add_test(tc_core, test_jwtsvid_EC);
     tcase_add_test(tc_core, test_jwtsvid_Marshal);
+    tcase_add_test(tc_core, test_jwtsvid_error5);
 
     suite_add_tcase(s, tc_core);
 
