54308da4bc330b04564d3f225d432b2137d535d2
Ariana <alg@cesar.org.br>
2021-04-15 15:03:16 -0300

    fix missing argument

--- integration_test/features/steps/mtls_steps.py
+++ integration_test/features/steps/mtls_steps.py
@@ -6,7 +6,7 @@ from hamcrest import assert_that, is_, is_not
 
 
 @given('The second agent is turned off inside "{container_name}" container')
-def step_impl(context):
+def step_impl(context, container_name):
     os.system('ssh root@%s << "EOL" pkill spire-agent EOL' % container_name)
     time.sleep(5)
 
