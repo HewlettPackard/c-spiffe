2e29237f91678994a5f275249fa596f95f7435e6
Ariana <alg@cesar.org.br>
2021-04-16 10:12:02 -0300

    rename script and add fingerprint configuration

---
+++ integration_test/helpers/bash-general-scripts/ssh-configure.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo -e '\n' | ssh-keygen -t rsa -P ''
+cat /root/.ssh/id_rsa.pub > /mnt/c-spiffe/integration_test/helpers/bash-general-scripts/authorized_keys
+mkdir ~/.ssh
+ssh -o "StrictHostKeyChecking no" workload << "EOL"
+exit
+EOL
--- integration_test/helpers/bash-general-scripts/ssh-generate-pub.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo -e '\n' | ssh-keygen -t rsa -P ''
-cat /root/.ssh/id_rsa.pub > /mnt/c-spiffe/integration_test/helpers/bash-general-scripts/authorized_keys
