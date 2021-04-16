c8d9c246b9276d1464faa977f53f52bb35ab1815
Ariana <alg@cesar.org.br>
2021-04-16 16:47:32 -0300

    update Makefile, remove comments and add blank line in the end of some files

--- integration_test/features/steps/mtls_steps.py
+++ integration_test/features/steps/mtls_steps.py
@@ -15,14 +15,3 @@ def step_impl(context, container_name):
 def step_impl(context, container_name):
     os.system("/mnt/c-spiffe/integration_test/helpers/bash-spire-scripts/ssh-connect_agent.sh %s" % container_name)
     time.sleep(5)
-
-
-# @when('I fetch external "{profile}" "{document}"')
-# def step_impl(context, profile, document):
-#     if document == "SVID":
-#         bin_file = "c_client"
-#     else:
-#         bin_file = "c_client_bundle"
-
-#     c_client_bin = os.popen("/mnt/c-spiffe/build/workload/%s %s_type=%s" % (bin_file, document.lower(), profile.lower()))
-#     context.result = c_client_bin.read()
