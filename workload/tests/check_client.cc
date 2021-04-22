a0d50767cc47ac9a236da32198b0f9add26a7151
rodrigolc <rodrigolpscarvalho@gmail.com>
2021-04-22 15:05:37 +0000

    fix: applied code-style

--- workload/tests/check_client.cc
+++ workload/tests/check_client.cc
@@ -7,11 +7,11 @@
  * Copyright (c) 2020 CESAR
  */
 
+#include "svid/x509svid/src/svid.h"
 #include "workload.grpc.pb.h"
 #include "workload.pb.h"
-#include "workload_mock.grpc.pb.h"
-#include "svid/x509svid/src/svid.h"
 #include "workload/src/client.h"
+#include "workload_mock.grpc.pb.h"
 #include <check.h>
 #include <gmock/gmock.h>
 #include <grpc/grpc.h>
@@ -359,27 +359,28 @@ START_TEST(test_workloadapi_Client_Connect_uses_stub)
     // normal constructor test
     workloadapi_Client *client = workloadapi_NewClient(&err);
     ck_assert_ptr_ne(client, NULL);
-    ck_assert_int_eq(err,NO_ERROR);
+    ck_assert_int_eq(err, NO_ERROR);
 
     MockSpiffeWorkloadAPIStub *stub = new MockSpiffeWorkloadAPIStub();
     err = workloadapi_Client_SetStub(client, stub);
-    
-    //set the owned flag so when we add a new stub the previous is deleted on SetStub
+
+    // set the owned flag so when we add a new stub the previous is deleted on
+    // SetStub
     client->owns_stub = true;
     stub = new MockSpiffeWorkloadAPIStub();
-    
-    err = workloadapi_Client_SetStub(client,(workloadapi_Stub*) 1);
-    ck_assert_int_eq(err, NO_ERROR);
-    ck_assert_ptr_eq(client->stub,(workloadapi_Stub*)1);
 
-    err = workloadapi_Client_SetStub(client,stub);
+    err = workloadapi_Client_SetStub(client, (workloadapi_Stub *) 1);
     ck_assert_int_eq(err, NO_ERROR);
-    ck_assert_ptr_eq(client->stub,stub);
+    ck_assert_ptr_eq(client->stub, (workloadapi_Stub *) 1);
 
-    //null test to setStub
-    err = workloadapi_Client_SetStub(NULL,NULL);
-    ck_assert_int_eq(err,ERROR1);
-    
+    err = workloadapi_Client_SetStub(client, stub);
+    ck_assert_int_eq(err, NO_ERROR);
+    ck_assert_ptr_eq(client->stub, stub);
+
+    // null test to setStub
+    err = workloadapi_Client_SetStub(NULL, NULL);
+    ck_assert_int_eq(err, ERROR1);
+
     // setAddress tests
     err = workloadapi_Client_SetAddress(NULL, NULL);
     ck_assert_int_eq(err, ERROR1);
@@ -400,10 +401,9 @@ START_TEST(test_workloadapi_Client_Connect_uses_stub)
     ck_assert_int_eq(err, NO_ERROR);
     ck_assert_ptr_ne(client->address, old_address);
 
-
     // setHeader tests
-    err = workloadapi_Client_AddHeader(NULL,NULL,NULL);
-    ck_assert_int_eq(err,ERROR1);
+    err = workloadapi_Client_AddHeader(NULL, NULL, NULL);
+    ck_assert_int_eq(err, ERROR1);
 
     workloadapi_Client_setDefaultHeaderOption(client, NULL);
 
@@ -435,14 +435,13 @@ START_TEST(test_workloadapi_parseJWTSVID_null_or_empty)
     ck_assert_ptr_eq(svid, NULL);
     ck_assert_int_eq(err, ERROR1); // NULL pointer error
 
-    ///check with an empty response (no svid)
-    JWTSVIDResponse* resp = new JWTSVIDResponse();
+    /// check with an empty response (no svid)
+    JWTSVIDResponse *resp = new JWTSVIDResponse();
     resp->clear_svids();
-    jwtsvid_Params params = {"audience1",NULL,NULL}; //empty params
+    jwtsvid_Params params = { "audience1", NULL, NULL }; // empty params
     svid = workloadapi_parseJWTSVID(resp, &params, &err);
-    ck_assert_ptr_eq(svid,NULL);
-    ck_assert_int_eq(err,ERROR2);
-
+    ck_assert_ptr_eq(svid, NULL);
+    ck_assert_int_eq(err, ERROR2);
 }
 END_TEST
 
@@ -453,7 +452,6 @@ START_TEST(test_workloadapi_parseJWTBundles_null_or_empty)
 
     ck_assert_ptr_eq(set, NULL);
     ck_assert_int_eq(err, ERROR1); // NULL pointer error
-
 }
 END_TEST
 
@@ -516,12 +514,11 @@ START_TEST(test_workloadapi_Client_FetchX509Bundles)
         .WillOnce(DoAll(WithArg<0>(set_single_SVID_response()), Return(true)));
     //   .WillOnce(Return(false));
 
-    x509bundle_Set *set
-        = workloadapi_Client_FetchX509Bundles(client, &err);
+    x509bundle_Set *set = workloadapi_Client_FetchX509Bundles(client, &err);
     workloadapi_Client_Close(client);
     workloadapi_Client_Free(client);
     ck_assert_ptr_ne(set, NULL);
-    ck_assert_int_eq(x509bundle_Set_Len(set), 2); //trust domain + federated
+    ck_assert_int_eq(x509bundle_Set_Len(set), 2); // trust domain + federated
     ck_assert_ptr_ne(set->bundles, NULL);
     ck_assert_str_eq(set->bundles[0].value[0].td.name, "example.org");
     ck_assert_str_eq(set->bundles[1].value[0].td.name, "federated.com");
@@ -879,31 +876,33 @@ START_TEST(test_workloadapi_Client_WatchX509Context)
 
     delete stub;
 
-    //NULL tests
+    // NULL tests
 
-    err = workloadapi_Client_watchX509Context(client,watcher,(workloadapi_Backoff*) NULL);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_watchX509Context(client, watcher,
+                                              (workloadapi_Backoff *) NULL);
+    ck_assert_int_eq(err, ERROR2);
 
     watcher = NULL;
-    err = workloadapi_Client_WatchX509Context(client,watcher);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_WatchX509Context(client, watcher);
+    ck_assert_int_eq(err, ERROR2);
 
-    err = workloadapi_Client_watchX509Context(client,watcher,(workloadapi_Backoff*) 0x1);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_watchX509Context(client, watcher,
+                                              (workloadapi_Backoff *) 0x1);
+    ck_assert_int_eq(err, ERROR2);
 
     client = NULL;
-    err = workloadapi_Client_WatchX509Context(client,watcher);
-    ck_assert_int_eq(err,ERROR1);
-    err = workloadapi_Client_watchX509Context(client,watcher,(workloadapi_Backoff*) 0x1);
-    ck_assert_int_eq(err,ERROR2);
-
+    err = workloadapi_Client_WatchX509Context(client, watcher);
+    ck_assert_int_eq(err, ERROR1);
+    err = workloadapi_Client_watchX509Context(client, watcher,
+                                              (workloadapi_Backoff *) 0x1);
+    ck_assert_int_eq(err, ERROR2);
 }
 END_TEST
 
 void callback_Watch_Jwtbundle_test(jwtbundle_Set *set, void *sets)
 {
-    jwtbundle_Set ***sets_ = (jwtbundle_Set***) sets;
-    jwtbundle_Set *newer = jwtbundle_Set_Clone((jwtbundle_Set*) set);
+    jwtbundle_Set ***sets_ = (jwtbundle_Set ***) sets;
+    jwtbundle_Set *newer = jwtbundle_Set_Clone((jwtbundle_Set *) set);
     arrpush((*sets_), newer);
 }
 
@@ -952,7 +951,7 @@ START_TEST(test_workloadapi_Client_WatchJWTBundles)
 
     workloadapi_Client_Close(client);
     workloadapi_Client_Free(client);
-    
+
     ck_assert_ptr_ne(sets, NULL);
     ck_assert_int_eq(arrlen(sets), 2);
 
@@ -971,26 +970,28 @@ START_TEST(test_workloadapi_Client_WatchJWTBundles)
 
     delete stub;
 
-    //NULL tests
+    // NULL tests
 
-    err = workloadapi_Client_watchJWTBundles(client,watcher,(workloadapi_Backoff*) NULL);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_watchJWTBundles(client, watcher,
+                                             (workloadapi_Backoff *) NULL);
+    ck_assert_int_eq(err, ERROR2);
 
     watcher = NULL;
-    err = workloadapi_Client_WatchJWTBundles(client,watcher);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_WatchJWTBundles(client, watcher);
+    ck_assert_int_eq(err, ERROR2);
 
-    err = workloadapi_Client_watchJWTBundles(client,watcher,(workloadapi_Backoff*) 0x1);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_watchJWTBundles(client, watcher,
+                                             (workloadapi_Backoff *) 0x1);
+    ck_assert_int_eq(err, ERROR2);
 
     client = NULL;
-    err = workloadapi_Client_WatchJWTBundles(client,watcher);
-    ck_assert_int_eq(err,ERROR1);
-    err = workloadapi_Client_watchJWTBundles(client,watcher,(workloadapi_Backoff*) 0x1);
-    ck_assert_int_eq(err,ERROR2);
+    err = workloadapi_Client_WatchJWTBundles(client, watcher);
+    ck_assert_int_eq(err, ERROR1);
+    err = workloadapi_Client_watchJWTBundles(client, watcher,
+                                             (workloadapi_Backoff *) 0x1);
+    ck_assert_int_eq(err, ERROR2);
 }
 END_TEST
-
 
 START_TEST(test_workloadapi_Client_FetchX509SVIDs)
 {
@@ -1015,15 +1016,14 @@ START_TEST(test_workloadapi_Client_FetchX509SVIDs)
         .WillOnce(DoAll(WithArg<0>(set_double_SVID_response()), Return(true)));
     //   .WillOnce(Return(false));
 
-    x509svid_SVID **svids
-        = workloadapi_Client_FetchX509SVIDs(client, &err);
+    x509svid_SVID **svids = workloadapi_Client_FetchX509SVIDs(client, &err);
     workloadapi_Client_Close(client);
     workloadapi_Client_Free(client);
 
     ck_assert_ptr_ne(svids, NULL);
     ck_assert_ptr_ne(svids[0], NULL);
     ck_assert_ptr_ne(svids[1], NULL);
-    ck_assert_ptr_ne(svids[0],svids[1]);
+    ck_assert_ptr_ne(svids[0], svids[1]);
     ck_assert_str_eq(svids[0]->id.path, "/workload-1");
     ck_assert_str_eq(svids[1]->id.path, "/workload-1");
 
@@ -1034,8 +1034,6 @@ START_TEST(test_workloadapi_Client_FetchX509SVIDs)
     arrfree(svids);
 }
 END_TEST
-
-
 
 Suite *client_suite(void)
 {
@@ -1048,7 +1046,8 @@ Suite *client_suite(void)
     tcase_add_test(tc_core, test_workloadapi_Client_Connect_uses_stub);
     tcase_add_test(tc_core, test_workloadapi_Client_Close);
     tcase_add_test(tc_core, test_workloadapi_Client_FetchX509Context);
-    tcase_add_test(tc_core, test_workloadapi_Client_FetchJWTBundles);    tcase_add_test(tc_core, test_workloadapi_Client_FetchX509Bundles);
+    tcase_add_test(tc_core, test_workloadapi_Client_FetchJWTBundles);
+    tcase_add_test(tc_core, test_workloadapi_Client_FetchX509Bundles);
     tcase_add_test(tc_core, test_workloadapi_Client_WatchX509Context);
     tcase_add_test(tc_core, test_workloadapi_Client_WatchJWTBundles);
     tcase_add_test(tc_core, test_workloadapi_Client_FetchX509SVIDs);
@@ -1056,7 +1055,6 @@ Suite *client_suite(void)
     tcase_add_test(tc_core, test_workloadapi_Client_ValidateJWTSVID);
     tcase_add_test(tc_core, test_workloadapi_parseJWTSVID_null_or_empty);
     tcase_add_test(tc_core, test_workloadapi_parseJWTBundles_null_or_empty);
-    
 
     suite_add_tcase(s, tc_core);
 
