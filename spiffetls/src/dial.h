bb71ac6c4f430f0d6cfeab86bdebb96d3fc8c99d
otaviolcs3 <olas@cin.ufpe.br>
2021-04-23 13:32:30 -0300

    docs: Documenting spiffetls dial

--- spiffetls/src/dial.h
+++ spiffetls/src/dial.h
@@ -8,6 +8,18 @@
 #include <openssl/ssl.h>
 #include <sys/socket.h>
 
+/**
+ * Establishes a TLS connection with given port, address, mode and
+ * configuration. If successful, returns a pointer to a established TLS
+ * connection object.
+ *
+ * \param port [in] Port number.
+ * \param addr [in] Address code.
+ * \param mode [in] Connection mode.
+ * \param config [in] Connection configuration.
+ * \param err [out] Variable to get information in the event of error.
+ * \returns Connected TLS object pointer if successful, NULL otherwise.
+ */
 SSL *spiffetls_DialWithMode(in_port_t port, in_addr_t addr,
                             spiffetls_DialMode *mode,
                             spiffetls_dialConfig *config, err_t *err);
