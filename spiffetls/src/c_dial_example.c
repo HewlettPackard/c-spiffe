c5316a5a06e923befb8ab21bf428dea16f810973
Willian Alves <wra@cesar.org.br>
2021-04-27 16:28:51 -0400

    Added validation for nullptr

--- spiffetls/src/c_dial_example.c
+++ spiffetls/src/c_dial_example.c
@@ -1,63 +1,127 @@
 #include "spiffetls/src/dial.h"
 #include <unistd.h>
 
+void init_openssl()
+{
+    SSL_load_error_strings();
+    //OpenSSL_add_ssl_algorithms();
+    SSL_library_init();
+}
+
 int main(void)
 {
+    init_openssl();
     err_t err;
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     workloadapi_Client *client = workloadapi_NewClient(&err);
-
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     if(err != NO_ERROR) {
         printf("client error! %d\n", (int) err);
         exit(-1);
     }
 
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     workloadapi_Client_defaultOptions(client, NULL);
+
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     err = workloadapi_Client_Connect(client);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     if(err != NO_ERROR) {
         printf("conn error! %d\n", (int) err);
         exit(-1);
     }
 
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     x509bundle_Set *set = workloadapi_Client_FetchX509Bundles(client, &err);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
 
     if(err != NO_ERROR) {
         printf("fetch error! %d\n", (int) err);
         exit(-1);
     }
 
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     x509bundle_Source *bundle_src = x509bundle_SourceFromSet(set);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
 
     spiffeid_TrustDomain td = { "example.org" };
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     tlsconfig_Authorizer *authorizer = tlsconfig_AuthorizeMemberOf(td);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
 
     spiffetls_DialMode *mode
         = spiffetls_TLSClientWithRawConfig(authorizer, bundle_src);
+
+    /* x509svid_SVID *svid
+        = x509svid_Load("./resources/good-leaf-and-intermediate.pem",
+                        "./resources/key-pkcs8-ecdsa.pem", &err);
+
+    x509svid_Source *svid_src = x509svid_SourceFromSVID(svid);
+
+     spiffetls_DialMode *mode
+        = spiffetls_MTLSClientWithRawConfig(authorizer, bundle_src, svid_src); */
+
     spiffetls_dialConfig config = { .base_TLS_conf = NULL, .dialer_fd = 0 };
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
 
     SSL *conn = spiffetls_DialWithMode((in_port_t) 4433,
                                        /*127.0.0.1*/ (in_addr_t) 0x7F000001,
                                        mode, &config, &err);
 
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
+
+    if (conn == NULL)
+    {
+        printf("spiffetls_DialWithMode() failed");
+        exit(-1);
+    }
+
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     if(err != NO_ERROR) {
         printf("could not create TLS connection!");
         exit(-1);
     }
 
+
+    char buff[1024];
+    int byytes;
+    SSL_write(conn, "will", sizeof("will"));
+    byytes = SSL_read(conn, buff, sizeof(buff)); /* get reply & decrypt */
+    printf("info: %s:%d: - SSL_read: %p\n", __FILE__, __LINE__, SSL_read);
+
+    buff[byytes] = 0;
+    printf("Received: \"%s\"\n", buff);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     err = workloadapi_Client_Close(client);
     if(err != NO_ERROR) {
         printf("close error! %d\n", (int) err);
     }
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     workloadapi_Client_Free(client);
     if(err != NO_ERROR) {
         printf("client free error! %d\n", (int) err);
     }
 
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     spiffetls_DialMode_Free(mode);
 
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+
+    char buf[1024];
+    int bytes;
+    printf("info: %s:%d: - SSL_read: %p\n", __FILE__, __LINE__, SSL_read);
+    bytes = SSL_read(conn, buf, sizeof(buf)); /* get reply & decrypt */
+
     const int fd = SSL_get_fd(conn);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     SSL_shutdown(conn);
+
     SSL_free(conn);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     close(fd);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
 
     return 0;
 }
--- spiffetls/src/dial.c
+++ spiffetls/src/dial.c
@@ -11,9 +11,14 @@ static int createSocket(in_addr_t addr, in_port_t port)
                                    .sin_port = htons(port) };
 
     const int sockfd = socket(/*IPv4*/ AF_INET, /*TCP*/ SOCK_STREAM, /*IP*/ 0);
-    const int connect_ret = connect(sockfd, (const struct sockaddr *) &address, sizeof address);
-    if(connect_ret < 0 || sockfd < 0) {
-        // could not create socket
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+    if (sockfd < 0) {
+        printf("ERROR opening socket");
+        return -1;
+    }
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+    if (connect(sockfd, (const struct sockaddr *) &address, sizeof(address)) < 0) {
+        printf("ERROR connecting");
         return -1;
     }
 
@@ -24,6 +29,12 @@ static SSL_CTX *createTLSContext()
 {
     const SSL_METHOD *method = TLS_method();
     SSL_CTX *ctx = SSL_CTX_new(method);
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+    if (!ctx)
+    {
+        printf("Unable to create SSL context");
+        return NULL;
+    }
 
     return ctx;
 }
@@ -32,6 +43,7 @@ SSL *spiffetls_DialWithMode(in_port_t port, in_addr_t addr,
                             spiffetls_DialMode *mode,
                             spiffetls_dialConfig *config, err_t *err)
 {
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     if(!mode->unneeded_source) {
         workloadapi_X509Source *source = mode->source;
         if(!source) {
@@ -48,46 +60,83 @@ SSL *spiffetls_DialWithMode(in_port_t port, in_addr_t addr,
 
     SSL_CTX *tls_config
         = config->base_TLS_conf ? config->base_TLS_conf : createTLSContext();
-
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     switch(mode->mode) {
     case TLS_CLIENT_MODE:
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         tlsconfig_HookTLSClientConfig(tls_config, mode->bundle,
                                       mode->authorizer, NULL);
         break;
     case MTLS_CLIENT_MODE:
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         tlsconfig_HookMTLSClientConfig(tls_config, mode->svid, mode->bundle,
                                        mode->authorizer, NULL);
         break;
     case MTLS_WEBCLIENT_MODE:
     default:
         // unknown mode
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         *err = ERROR1;
         goto error;
     }
-
-    SSL *conn = SSL_new(tls_config);
-
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
     const int sockfd
         = config->dialer_fd > 0 ? config->dialer_fd : createSocket(addr, port);
 
     if(sockfd < 0) {
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         // could not create socket with given address and port
         *err = ERROR1;
         goto error;
     }
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+    SSL *conn = SSL_new(tls_config);
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
+
+    if (!conn)
+    {
+        printf("SSL_new() failed\n");
+        goto error;
+    }
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
+
+    if (SSL_set_fd(conn, sockfd) != 1)
+    {
+        printf("Failed to SSL_set_fd\n");
+        goto error;
+    }
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
 
     SSL_set_fd(conn, sockfd);
+
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
+
     SSL_set_connect_state(conn);
+
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
 
     if(SSL_connect(conn) != 1) {
         // could not build a SSL session
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         SSL_shutdown(conn);
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         SSL_free(conn);
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
         close(sockfd);
-        *err = ERROR2;
+        printf("info: %s:%d: \n", __FILE__, __LINE__);
+        *err = ERROR3;
         goto error;
     }
-    // successful handshake
+
+    printf("info: %s:%d: - Conn: %p\n", __FILE__, __LINE__, conn);
+
+    printf("info: %s:%d: \n", __FILE__, __LINE__);
+
+    printf("Connected with %s encryption\n", SSL_get_cipher(conn));
+
+     // successful handshake
     return conn;
 
 error:
