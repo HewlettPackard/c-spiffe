47c502418a0cb8bc255e852e9412efc35649eba6
Ariana <alg@cesar.org.br>
2021-04-29 09:47:59 -0300

    update go server bash scripts

--- integration_test/helpers/bash-general-scripts/ssh-run-go-server.sh
+++ integration_test/helpers/bash-general-scripts/ssh-run-go-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ssh root@$1 << "EOL" 
 cd /mnt/c-spiffe/integration_test/helpers/go-echo-server/server
-go run server.go > /dev/null 2>&1 &
+su - server-workload -c "./go-server" > /dev/null 2>&1 &
 EOL
--- integration_test/helpers/bash-general-scripts/ssh-stop-go-server.sh
+++ integration_test/helpers/bash-general-scripts/ssh-stop-go-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ssh root@$1 << "EOL" 
-kill -9 `ps aux | grep go | awk '{print $2}'`
+pkill go-server
 EOL
