0e040c135d9ae036a9c272a19b28ff2fa4929af7
Ariana <alg@cesar.org.br>
2021-05-13 14:52:48 -0300

    bash scripts refactor, improvements and updates for MT_002

--- integration_test/helpers/bash-spire-scripts/ssh-create-entries.sh
+++ integration_test/helpers/bash-spire-scripts/ssh-create-entries.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+#arguments: $1 = hostname numeration (optional)
 if [ $1 ];
 then
-    ssh root@$1 << "EOL" 
+    ssh root@spire-server$1 << "EOL" 
     spire-server entry create -parentID spiffe://example2.org/myagent -spiffeID spiffe://example2.org/myworkloadC -selector unix:user:server-workload
 EOL
-else;
-spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkloadA -selector unix:user:client-workload
-spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkloadA -selector unix:user:root
-spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkloadB -selector unix:user:server-workload
+else
+    ssh root@spire-server << "EOL" 
+    spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkloadA -selector unix:user:client-workload
+    spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkloadA -selector unix:user:root
+    spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myworkloadB -selector unix:user:server-workload
+EOL
 fi
--- integration_test/helpers/bash-spire-scripts/ssh-start-server.sh
+++ integration_test/helpers/bash-spire-scripts/ssh-start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#arguments: $1 = hostname numeration (optional)
 if [ $1 ];
 then 
-    ssh root@$1 <<EOL
+    ssh root@spire-server$1 <<EOL
     spire-server run -config /opt/spire/conf/server/server.conf> /dev/null 2>&1 &
 EOL
 else
