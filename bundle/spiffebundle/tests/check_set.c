b80ef24df28d60226950641db3a43942e612135e
otaviolcs3 <olas@cin.ufpe.br>
2021-05-18 16:22:48 -0300

    Testing two more functions

--- bundle/spiffebundle/tests/check_set.c
+++ bundle/spiffebundle/tests/check_set.c
@@ -115,6 +115,28 @@ START_TEST(test_spiffebundle_Set_Get)
 }
 END_TEST
 
+START_TEST(test_spiffebundle_Set_Bundles)
+{
+    const int ITERS = 2;
+
+    spiffeid_TrustDomain td[] = { "example.com", "example.org" };
+    err_t err;
+    spiffebundle_Bundle *bundles[ITERS];
+    bundles[0]
+        = spiffebundle_Load(td[0], "./resources/jwks_valid_1.json", &err);
+    bundles[1]
+        = spiffebundle_Load(td[1], "./resources/jwks_valid_2.json", &err);
+    spiffebundle_Set *set = spiffebundle_NewSet(2, bundles[0], bundles[1]);
+    spiffebundle_Bundle **arr_bundles = spiffebundle_Set_Bundles(set);
+
+    ck_assert_ptr_ne(bundles, NULL);
+    ck_assert_uint_eq(arrlenu(bundles), spiffebundle_Set_Len(set));
+
+    spiffebundle_Set_Free(set);
+    arrfree(arr_bundles);
+}
+END_TEST
+
 START_TEST(test_spiffebundle_Set_GetBundleForTrustDomain)
 {
     const int ITERS = 2;
@@ -236,6 +258,7 @@ Suite *set_suite(void)
     tcase_add_test(tc_core, test_spiffebundle_Set_Add);
     tcase_add_test(tc_core, test_spiffebundle_Set_Remove);
     tcase_add_test(tc_core, test_spiffebundle_Set_Get);
+    tcase_add_test(tc_core, test_spiffebundle_Set_Bundles);
     tcase_add_test(tc_core, test_spiffebundle_Set_GetBundleForTrustDomain);
     tcase_add_test(tc_core, test_spiffebundle_Set_GetX509BundleForTrustDomain);
     tcase_add_test(tc_core, test_spiffebundle_Set_GetJWTBundleForTrustDomain);
