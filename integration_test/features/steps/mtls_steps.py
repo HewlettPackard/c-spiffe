e8ea6c693a6f168bab5ff12ef39709ac2cf92b98
Ariana <alg@cesar.org.br>
2021-05-19 16:03:40 -0300

    replace strings for constants

--- integration_test/features/steps/mtls_steps.py
+++ integration_test/features/steps/mtls_steps.py
@@ -57,7 +57,7 @@ def step_impl(context, message, container_name, language):
         time.sleep(1)
         result = client.stderr
     elif language == "c":
-        client = os.popen("/mnt/c-spiffe/build/spiffetls/c_dial '%s' %s 4433 example.org" % (message, socket.gethostbyname(container_name)))
+        client = os.popen("/mnt/c-spiffe/build/spiffetls/c_dial '%s' %s %s %s" % (message, socket.gethostbyname(container_name), context.default_echo_server_port, context.default_trust_domain))
         result = client.read()
     context.result = result
 
