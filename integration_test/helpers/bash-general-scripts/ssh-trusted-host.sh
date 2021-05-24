141b15b3544ac5a710a617c2005f19aa03943f03
Ariana <alg@cesar.org.br>
2021-05-24 11:53:00 -0300

    Revert "update infra to have a second workload container"
    This reverts commit b9ad9dbeb0e92731be0c2480b8398dd4741755cc.

--- integration_test/helpers/bash-general-scripts/ssh-trusted-host.sh
+++ integration_test/helpers/bash-general-scripts/ssh-trusted-host.sh
@@ -3,9 +3,6 @@ mkdir ~/.ssh
 ssh -o "StrictHostKeyChecking no" workload << "EOL"
 exit
 EOL
-ssh -o "StrictHostKeyChecking no" workload2 << "EOL"
-exit
-EOL
 ssh -o "StrictHostKeyChecking no" spire-server2 << "EOL"
 exit
 EOL
