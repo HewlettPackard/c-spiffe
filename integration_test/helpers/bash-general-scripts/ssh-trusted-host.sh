867303d3c733036fe25a74a41aa0e6ccd3daad50
Ariana <alg@cesar.org.br>
2021-06-01 09:36:17 -0300

    update bash scripts

--- integration_test/helpers/bash-general-scripts/ssh-trusted-host.sh
+++ integration_test/helpers/bash-general-scripts/ssh-trusted-host.sh
@@ -6,6 +6,9 @@ EOL
 ssh -o "StrictHostKeyChecking no" workload2 << "EOL"
 exit
 EOL
+ssh -o "StrictHostKeyChecking no" spire-server << "EOL"
+exit
+EOL
 ssh -o "StrictHostKeyChecking no" spire-server2 << "EOL"
 exit
 EOL
--- integration_test/helpers/bash-spire-scripts/ssh-connect-agent.sh
+++ integration_test/helpers/bash-spire-scripts/ssh-connect-agent.sh
@@ -3,7 +3,7 @@ TOKEN=$(awk '{ print $2 }' /mnt/c-spiffe/integration_test/myagent$1.token)
 if [ $2 ];
 then
 ssh root@$2 <<EOL
-    nohup spire-agent run -joinToken ${TOKEN} -config /opt/spire/conf/agent/agent.conf> /dev/null 2>&1 & 
+    nohup spire-agent run -joinToken ${TOKEN} -config /opt/spire/conf/agent/agent${1}.conf> /dev/null 2>&1 & 
 EOL
 else
     spire-agent run -joinToken $TOKEN -config /opt/spire/conf/agent/agent.conf &
