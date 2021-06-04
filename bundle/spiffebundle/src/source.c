d1a144753636fffc980710cdfe642b85bb4b25ef
rodrigolc <rodrigolpscarvalho@gmail.com>
2021-06-04 12:19:04 +0000

    tests: spiffebundle_Source tests and cmake files

--- bundle/spiffebundle/src/source.c
+++ bundle/spiffebundle/src/source.c
@@ -3,17 +3,19 @@
 spiffebundle_Bundle *spiffebundle_Source_GetSpiffeBundleForTrustDomain(
     spiffebundle_Source *s, const spiffeid_TrustDomain td, err_t *err)
 {
-    if(s->type == SPIFFEBUNDLE_BUNDLE) {
-        return spiffebundle_Bundle_GetBundleForTrustDomain(s->source.bundle,
-                                                           td, err);
-    } else if(s->type == SPIFFEBUNDLE_SET) {
-        return spiffebundle_Set_GetBundleForTrustDomain(s->source.set, td,
-                                                        err);
-    } else if(s->type == SPIFFEBUNDLE_ENDPOINT) {
-        return spiffebundle_Endpoint_GetBundleForTrustDomain(
-            s->source.endpoint, td, err);
+    if(s) {
+        if(s->type == SPIFFEBUNDLE_BUNDLE) {
+            return spiffebundle_Bundle_GetBundleForTrustDomain(
+                s->source.bundle, td, err);
+        } else if(s->type == SPIFFEBUNDLE_SET) {
+            return spiffebundle_Set_GetBundleForTrustDomain(s->source.set, td,
+                                                            err);
+        } else if(s->type == SPIFFEBUNDLE_ENDPOINT) {
+            return spiffebundle_Endpoint_GetBundleForTrustDomain(
+                s->source.endpoint, td, err);
+        }
     }
-
+    *err = ERROR1;
     return NULL;
 }
 
