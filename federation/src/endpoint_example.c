58851cece77f1a5de92f3537812da3a3933ba989
rodrigolc <rodrigolpscarvalho@gmail.com>
2021-06-04 12:19:04 +0000

    docs: added comments and cleaned example

--- federation/src/endpoint_example.c
+++ federation/src/endpoint_example.c
@@ -4,22 +4,40 @@
 int main()
 {
     err_t err = NO_ERROR;
+    // Create Endpoint proxy object.
     spiffebundle_Endpoint *endpoint = spiffebundle_Endpoint_New();
-    /// TODO: find a spiffe bundle endpoint.
+
+    // Configure Endpoint
+    // since raw.githubusercontent.com has a bundle installed for it, 
+    // the HTTPS call will authenticate.
     spiffebundle_Endpoint_Config_HTTPS_WEB(
         endpoint,
         "https://raw.githubusercontent.com/HewlettPackard/c-spiffe/master/"
         "bundle/jwtbundle/tests/resources/jwk_keys.json",
         spiffeid_TrustDomainFromString("example.com", &err));
-
+    // Fetch it.
     err = spiffebundle_Endpoint_Fetch(endpoint);
+
+    // Get it.
     spiffebundle_Bundle *bundle
         = spiffebundle_Endpoint_GetBundleForTrustDomain(
             endpoint, spiffeid_TrustDomainFromString("example.com", &err),
             &err);
 
-    printf("pBundle: %p\n", bundle);
+    // Marshall it into a json string
+    string_t jwks = spiffebundle_Bundle_Marshal(bundle, &err);
 
-    printf("td: %s\n", bundle->td.name);
+    printf("Trust Domain: %s\n", bundle->td.name);
+    printf("Bundle for TD: %s\n", jwks);
+
+    // Free string.
+    util_string_t_Free(jwks);
+
+    // don't free spiffebundle since endpoint owns it
+    // spiffebundle_Bundle_Free(bundle);
+
+    // Free endpoint
+    spiffebundle_Endpoint_Free(endpoint);
+
     return 0;
 }
