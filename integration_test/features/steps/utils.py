a8748499509e6876de07823a80e38d4b55219837
Ariana <alg@cesar.org.br>
2021-06-09 11:07:26 -0300

    add method to remove entry

--- integration_test/features/steps/utils.py
+++ integration_test/features/steps/utils.py
@@ -17,8 +17,21 @@ def parse_optional(text):
     return text.strip()
 
 
+def list_entries(container):
+    return os.popen("ssh root@%s \"spire-server entry show\"" % container).read()
+
+
 def is_wlc_entry_created(container):
-    entries = os.popen("ssh root@%s \"spire-server entry show\"" % container).read()
+    entries = list_entries(container)
     if entries.find("myworkloadC") == -1:
         return False
     return True
+
+
+def remove_entry(workload_id, container):
+    workload = "myworkload%s" % workload_id[-1]
+    entries = list_entries(container).splitlines()
+    for entry in entries:
+        if entry.find(workload) != -1:
+            entryId = ""
+            os.system("ssh root@%s \"spire-server entry delete -entryId %s\"" % (container, entryId))
