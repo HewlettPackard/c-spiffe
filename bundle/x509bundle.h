b61970be0418681a5d5f44ac2bf60b25741e2723
otaviolcs3 <olas@cin.ufpe.br>
2021-06-11 11:56:12 -0300

    Adding higher level headers

---
+++ bundle/x509bundle.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_X509BUNDLE_H
+#define INCLUDE_X509BUNDLE_H
+
+#include "bundle/x509bundle/bundle.h"
+#include "bundle/x509bundle/set.h"
+#include "bundle/x509bundle/source.h"
+
+#endif
---
+++ federation/federation.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_FEDERATION_H
+#define INCLUDE_FEDERATION_H
+
+#include "federation/endpoint.h"
+#include "federation/federation.h"
+#include "federation/watcher.h"
+
+#endif
