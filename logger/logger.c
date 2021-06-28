377b87f8831b2e9eca2528d0b4b0a7a61b5bab5d
otaviolcs3 <olas@cin.ufpe.br>
2021-06-28 16:16:03 -0300

    Dev

--- logger/logger.c
+++ logger/logger.c
@@ -4,8 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const size_t MAX_LOGGER_CAP = 256;
-string_t __str_debug;
+const size_t MAX_LOGGER_CAP = 1 << 7;
+const size_t MAX_STR_CAP = 1 << 8;
+string_arr_t __str_debug;
+
+const char *DEBUG_PREFIX = "[DEBUG] ";
 
 void logger_Init(void)
 {
@@ -13,17 +16,22 @@ void logger_Init(void)
     arrsetcap(__str_debug, MAX_LOGGER_CAP);
 }
 
-void logger_Debugf(const char *fmt, ...)
+void logger_Debug_FmtPush(const char *fmt, ...)
 {
-    if(__str_debug) {
+    if(__str_debug && fmt) {
         // debug_fmt = "[DEBUG] " + fmt + "\n";
-        string_t debug_fmt = string_new("[DEBUG] ");
+        string_t debug_fmt = string_new(DEBUG_PREFIX);
         debug_fmt = string_push(debug_fmt, fmt);
         debug_fmt = string_push(debug_fmt, "\n");
 
         va_list args;
         va_start(args, fmt);
-        vsnprintf(__str_debug, arrlenu(__str_debug), debug_fmt, &args);
+
+        string_t new_str = NULL;
+        arrsetcap(new_str, MAX_STR_CAP);
+        vsnprintf(new_str, arrcap(new_str), debug_fmt, &args);
+        arrsetlen(new_str, strlen(new_str) + 1);
+        arrpush(__str_debug, new_str);
 
         arrfree(debug_fmt);
 
@@ -31,10 +39,57 @@ void logger_Debugf(const char *fmt, ...)
     }
 }
 
+void logger_Debug_Push(const char *str)
+{
+    if(__str_debug && str) {
+        string_t new_str = string_new(DEBUG_PREFIX);
+        /// TODO: set max capacity?
+        new_str = string_push(new_str, str);
+        new_str = string_push(new_str, "\n");
+
+        arrpush(__str_debug, new_str);
+    }
+}
+
+const char *logger_Debug_Back(void)
+{
+    const size_t len = arrlenu(__str_debug);
+    if(len > 0) {
+        return __str_debug[len - 1];
+    }
+
+    return NULL;
+}
+
+const char *logger_Debug_Pop(void)
+{
+    if(arrlenu(__str_debug) > 0) {
+        return arrpop(__str_debug);
+    }
+
+    return NULL;
+}
+
+const char **logger_Debug_Traceback(bool clean)
+{
+    // dummy
+    return NULL;
+}
+
+void logger_Debug_Dumpf(FILE *f)
+{
+    // dummy
+}
+
+string_t logger_Debug_Dumps(void)
+{
+    // dummy
+    return NULL;
+}
+
 void logger_Cleanup(void)
 {
     /// TODO: close all loggers here
-    if(__file_debug) {
-        fclose(__file_debug);
-    }
+    util_string_arr_t_Free(__str_debug);
+    
 }
--- logger/logger.h
+++ logger/logger.h
@@ -1,16 +1,40 @@
 #ifndef INCLUDE_LOGGER_LOGGER_H
 #define INCLUDE_LOGGER_LOGGER_H
 
-void logger_InitLogger(void);
+#include <stdio.h>
 
-void logger_Debugf();
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void logger_Infof();
+void logger_InitLoggers(void);
 
-void logger_Warnf();
+void loffer_Debug_Init(void);
 
-void logger_Errorf();
+void logger_Debug_FmtPush(const char *fmt, ...);
+
+void loffer_Debug_Push(const char *str);
+
+const char *logger_Debug_Back(void);
+
+const char *logger_Debug_Pop(void);
+
+const char **logger_Debug_Traceback(bool clean);
+
+void logger_Debug_Dumpf(FILE *f);
+
+string_t logger_Debug_Dumps(void);
+
+void logger_Info_Init();
+
+void logger_Warn_Init();
+
+void logger_Error_Init();
 
 void logger_Cleanup(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
