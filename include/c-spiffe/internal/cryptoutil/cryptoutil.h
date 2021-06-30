058d62fbc2b99f80b32acb657d0f9e157ad7c4fa
Willian Alves <wra@cesar.org.br>
2021-06-29 21:51:38 -0400

    Removed files unnecessary

--- include/c-spiffe/internal/cryptoutil/cryptoutil.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_INTERNAL_CRYPTOUTIL_H
-#define INCLUDE_INTERNAL_CRYPTOUTIL_H
-
-#include "c-spiffe/internal/cryptoutil/keys.h"
-
-#endif
--- include/c-spiffe/internal/pemutil/pemutil.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_INTERNAL_PEMUTIL_H
-#define INCLUDE_INTERNAL_PEMUTIL_H
-
-#include "c-spiffe/internal/pemutil/pem.h"
-
-#endif
