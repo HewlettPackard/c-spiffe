1b26081284835adaeab9adf94bcedffb1de858f8
Ariana <alg@cesar.org.br>
2021-07-06 08:37:50 -0300

    force process to stop

--- integration_test/helpers/bash-spire-scripts/ssh-stop-process.sh
+++ integration_test/helpers/bash-spire-scripts/ssh-stop-process.sh
@@ -3,7 +3,7 @@
 if [ $3 ];
 then
 TOKEN=$(awk '{ print $2 }' /mnt/c-spiffe/integration_test/myagent${3}.token)
-ssh root@$2 "ps aux | grep spire-${1} | grep ${TOKEN} | awk '{ print \$2 }' | xargs -I {} kill {}"
+ssh root@$2 "ps aux | grep spire-${1} | grep ${TOKEN} | awk '{ print \$2 }' | xargs -I {} kill -9 {}"
 else
-ssh root@$2 "pkill spire-$1"
+ssh root@$2 "pkill -9 spire-$1"
 fi
