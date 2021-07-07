8a078adb5701824ae3c27204c9ad020cdd9a4c23
Ariana <alg@cesar.org.br>
2021-07-07 17:04:22 -0300

    fix argument

--- integration_test/helpers/bash-general-scripts/ssh-run-go-server.sh
+++ integration_test/helpers/bash-general-scripts/ssh-run-go-server.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 #arguments: $1 = hostname; $2 = port
-ssh root@$1 << "EOL" 
-cd /mnt/c-spiffe/integration_test/helpers/go-echo-server/server
-su - server-workload -c "./go-server ${2}" > /mnt/c-spiffe/integration_test/go-listen.log 2>&1 &
-EOL
+ssh root@$1 "cd /mnt/c-spiffe/integration_test/helpers/go-echo-server/server; su - server-workload -c \"./go-server ${2}\" > /dev/null 2>&1 &"
