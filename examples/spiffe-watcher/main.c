fbe79a58df646703e48bdc7fc6be2a628f36d067
otaviolcs3 <olas@cin.ufpe.br>
2021-08-05 15:31:49 -0300

    Dev

--- examples/spiffe-watcher/main.c
+++ examples/spiffe-watcher/main.c
@@ -2,13 +2,24 @@
 #include "c-spiffe/workload/workload.h"
 #include "threads.h"
 
+void callback_X509Context(workloadapi_X509Context *ctx, void *unused)
+{
+    // dummy
+}
+
+void callback_JWTBundles(jwtbundle_Set *set, void *unused)
+{
+    // dummy
+}
+
 int watch_X509SVIDs(void *arg)
 {
     workloadapi_Client *client = arg;
     /// TODO: configure watcher
     workloadapi_WatcherConfig config
         = { .client = client, .client_options = NULL };
-    workloadapi_X509Callback cb = { .args = NULL, .func = NULL };
+    workloadapi_X509Callback cb
+        = { .args = NULL, .func = callback_X509Context };
     err_t err;
     workloadapi_Watcher *watcher = workloadapi_newWatcher(config, cb, &err);
     if(!watcher || err) {
@@ -36,7 +47,7 @@ int watch_JWTBundles(void *arg)
     /// TODO: configure watcher
     workloadapi_JWTWatcherConfig config
         = { .client = client, .client_options = NULL };
-    workloadapi_JWTCallback cb = { .args = NULL, .func = NULL };
+    workloadapi_JWTCallback cb = { .args = NULL, .func = callback_JWTBundles };
     err_t err;
     workloadapi_JWTWatcher *watcher
         = workloadapi_newJWTWatcher(config, cb, &err);
@@ -68,7 +79,13 @@ void startWatchers(void)
         printf("Failed creating client: %u\n", err);
         return;
     }
-    /// TODO: configure client
+
+    workloadapi_Client_defaultOptions(client, NULL);
+    err = workloadapi_Client_Connect(client);
+    if(err) {
+        printf("Failed connecting client: %u\n", err);
+        return;
+    }
 
     thrd_create(&thrd_x509, watch_X509SVIDs, client);
     thrd_create(&thrd_jwt, watch_JWTBundles, client);
