64c98f737dd677844c988c1ff5d283a4d7180905
otaviolcs3 <olas@cin.ufpe.br>
2021-08-05 12:47:59 -0300

    Dev and code-style

--- examples/spiffe-watcher/main.c
+++ examples/spiffe-watcher/main.c
@@ -6,16 +6,26 @@ int watch_X509SVIDs(void *arg)
 {
     workloadapi_Client *client = arg;
     /// TODO: configure watcher
-    workloadapi_WatcherConfig config = { .client = client, .client_options = NULL };
+    workloadapi_WatcherConfig config
+        = { .client = client, .client_options = NULL };
     workloadapi_X509Callback cb = { .args = NULL, .func = NULL };
     err_t err;
     workloadapi_Watcher *watcher = workloadapi_newWatcher(config, cb, &err);
+    if(!watcher || err) {
+        printf("Failed creating X.509 watcher: %u\n", err);
+        return -1;
+    }
 
     err = workloadapi_Client_WatchX509Context(client, watcher);
-
     if(err) {
         printf("Failed watching X.509 context: %u\n", err);
     }
+
+    err = workloadapi_Watcher_Close(watcher);
+    if(err) {
+        printf("Failed closing X.509 watcher: %u\n", err);
+    }
+    workloadapi_Watcher_Free(watcher);
 
     return 0;
 }
@@ -24,16 +34,27 @@ int watch_JWTBundles(void *arg)
 {
     workloadapi_Client *client = arg;
     /// TODO: configure watcher
-    workloadapi_JWTWatcherConfig config = { .client = client, .client_options = NULL };
-    workloadapi_JWTCallback cv = { .args = NULL, .func = NULL };
+    workloadapi_JWTWatcherConfig config
+        = { .client = client, .client_options = NULL };
+    workloadapi_JWTCallback cb = { .args = NULL, .func = NULL };
     err_t err;
-    workloadapi_JWTWatcher *watcher = workloadapi_newJWTWatcher(config, cb, &err);
+    workloadapi_JWTWatcher *watcher
+        = workloadapi_newJWTWatcher(config, cb, &err);
+    if(!watcher || err) {
+        printf("Failed creating JWT watcher: %u\n", err);
+        return -1;
+    }
 
     err = workloadapi_Client_WatchJWTBundles(client, watcher);
-
     if(err) {
         printf("Failed watching JWT bundles: %u\n", err);
     }
+
+    err = workloadapi_JWTWatcher_Close(watcher);
+    if(err) {
+        printf("Failed closing JWT watcher: %u\n", err);
+    }
+    workloadapi_JWTWatcher_Free(watcher);
 
     return 0;
 }
@@ -48,7 +69,7 @@ void startWatchers(void)
         return;
     }
     /// TODO: configure client
-    
+
     thrd_create(&thrd_x509, watch_X509SVIDs, client);
     thrd_create(&thrd_jwt, watch_JWTBundles, client);
 
@@ -56,11 +77,9 @@ void startWatchers(void)
     thrd_join(&thrd_jwt, NULL);
 
     err = workloadapi_Client_Close(client);
-
     if(err) {
-        printf("Failed closint client: %u\n", err);
+        printf("Failed closing client: %u\n", err);
     }
-
     workloadapi_Client_Free(client);
 }
 
